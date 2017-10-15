9aa1795c47eb894a8541b3221924ee212363d728
Claas Lorenz <clorenz@genua.de>
2017-10-15 21:48:13 +0200

    Test reachability in ad6 workload script.

--- fave/test-workload-ad6.sh
+++ fave/test-workload-ad6.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-NPADDR=127.0.0.1
-NPPORT=1234
 
 TMPDIR=/tmp/np
 mkdir -p $TMPDIR
@@ -11,34 +9,35 @@ PFRLOG=$TMPDIR/pfr.log
 SUBLOG=$TMPDIR/sub.log
 SUBLLOG=$TMPDIR/subl.log
 SWLOG=$TMPDIR/sw.log
+SRCLOG=$TMPDIR/source.log
+SRCLLOG=$TMPDIR/sourcel.log
+PROBELOG=$TMPDIR/probe.log
+PROBELLOG=$TMPDIR/probel.log
 
-FORMAT="%e"
+TIME='/usr/bin/time -f %e'
 
-alias time='/usr/bin/time -f $FORMAT'
+echo -n "delete old logs and measurements... "
+rm -f /tmp/np/*.log
+echo "ok"
 
 echo -n "start netplumber... "
-TMPFILE=$TMPDIR/np.log
-net_plumber --hdr-len 1 --server $NPADDR $NPPORT > $TMPFILE &
-NP=$!
-sleep 1
+scripts/start_np.sh test-workload-ad6.conf
 echo "ok"
 
 echo -n "start aggregator... "
-PYTHONPATH=. python2 aggregator/aggregator.py &
-AGGR=$!
-sleep 1
+scripts/start_aggr.sh
 echo "ok"
 
 # build topology
 echo "read topology..."
 echo -n "create pgf... "
-PYTHONPATH=. time -o $PFLOG python2 topology/topology.py -a -t packet_filter -n pgf -i 2001:db8:abc::1 -p 24
+PYTHONPATH=. $TIME -o $PFLOG python2 topology/topology.py -a -t packet_filter -n pgf -i 2001:db8:abc::1 -p 24
 echo "ok"
 
 # create dmz
 echo -n "create dmz... "
-PYTHONPATH=. time -o $SUBLOG  python2 topology/topology.py -a -t switch -n dmz -p 9
-PYTHONPATH=. time -o $SUBLLOG python2 topology/topology.py -a -l pgf.2:dmz.1,dmz.1:pgf.2
+PYTHONPATH=. $TIME -o $SUBLOG  python2 topology/topology.py -a -t switch -n dmz -p 9
+PYTHONPATH=. $TIME -o $SUBLLOG python2 topology/topology.py -a -l pgf.2:dmz.1,dmz.1:pgf.2
 
 
 HOSTS="file,2001:db8:abc:0::1,tcp:21,tcp:115,tcp:22,udp:22 \
@@ -51,25 +50,13 @@ tcp:993,tcp:995,udp:143,udp:220,tcp:22,udp:22 \
     data,2001:db8:abc:0::7,tcp:118,tcp:156,tcp:22,udp:118,udp:156,udp:22 \
     adm,2001:db8:abc:0::8,udp:161,tcp:22,udp:22"
 
-cnt=2
-for  HOST in $HOSTS; do
-    H=`echo $HOST | cut -d ',' -f 1`
-    A=`echo $HOST | cut -d ',' -f 2`
-    P=`echo $HOST | cut -d ',' -f 3-`
-
-    PYTHONPATH=. python2 topology/topology.py -a -t host -n $H -i $A -u $P
-    PYTHONPATH=. python2 topology/topology.py -a -l $H.1:dmz.$cnt,dmz.$cnt:$H.1
-
-    cnt=$(($cnt+1))
-done
-
 echo "ok"
 
 # create wifi
 echo -n "create wifi... "
-PYTHONPATH=. time -ao $SUBLOG python2 topology/topology.py -a -t switch -n wifi -p 2
-PYTHONPATH=. time -ao $SUBLLOG python2 topology/topology.py -a -l pgf.3:wifi.1,wifi.1:pgf.3
-PYTHONPATH=. time -ao $SUBLOG python2 topology/topology.py -a -t generator -n wifi-clients -f ipv6_src=2001:db8:abc:1::0/64
+PYTHONPATH=. $TIME -ao $SUBLOG python2 topology/topology.py -a -t switch -n wifi -p 2
+PYTHONPATH=. $TIME -ao $SUBLLOG python2 topology/topology.py -a -l pgf.3:wifi.1,wifi.1:pgf.3
+#PYTHONPATH=. $TIME -ao $SUBLOG python2 topology/topology.py -a -t generator -n wifi-clients -f ipv6_src=2001:db8:abc:1::0/64
 echo "ok"
 
 # create subnets
@@ -105,26 +92,13 @@ udp:143,udp:220,udp:22 \
 
 cnt=4
 for NET in $SUBNETS; do
-    echo -n "create subnet $NET... "
+    echo -n "  create subnet $NET... "
 
     # create switch for subnet
-    PYTHONPATH=. time -ao $SUBLOG python2 topology/topology.py -at switch -n $NET -p 7
+    PYTHONPATH=. $TIME -ao $SUBLOG python2 topology/topology.py -at switch -n $NET -p 7
 
     # link switch to firewall
-    PYTHONPATH=. time -ao $SUBLLOG python2 topology/topology.py -al pgf.$cnt:$NET.1,$NET.1:pgf.$cnt
-
-    srv=1
-    for HOST in $SUBHOSTS; do
-        port=$(($srv+1))
-        SRV=`echo $HOST | cut -d ',' -f 1`.$NET
-        ADDR=2001:db8:abc:$cnt::$srv
-        PORTS=`echo $HOST | cut -d ',' -f 2-`
-
-        PYTHONPATH=. time -ao $SUBLOG python2 topology/topology.py -at host -n $SRV -i $ADDR -u $PORTS
-
-        # link switch to server
-        PYTHONPATH=. time -ao $SUBLLOG python2 topology/topology.py -al $SRV.1:$NET.$port,$NET.$port:$SRV.1
-    done
+    PYTHONPATH=. $TIME -ao $SUBLLOG python2 topology/topology.py -al pgf.$cnt:$NET.1,$NET.1:pgf.$cnt
 
     echo "ok"
 
@@ -134,18 +108,18 @@ done
 
 # populate firewall
 echo -n "populate firewall... "
-PYTHONPATH=. time -o $PFRLOG python2 ip6np/ip6np.py -n pgf -i 2001:db8:abc::1 -f pgf-ruleset
+PYTHONPATH=. $TIME -o $PFRLOG python2 ip6np/ip6np.py -n pgf -i 2001:db8:abc::1 -f pgf-ruleset
 
-# dmz
-PYTHONPATH=. time -o $SWLOG python2 openflow/switch.py -a -i 1 -n pgf -t 1 -f ipv6_dst=2001:db8:abc:0::0/64 -c fd=pgf.2
+# dmz (route)
+PYTHONPATH=. $TIME -o $SWLOG python2 openflow/switch.py -a -i 1 -n pgf -t 1 -f ipv6_dst=2001:db8:abc:0::0/64 -c fd=pgf.2
 
-# wifi
-PYTHONPATH=. time -ao $SWLOG python2 openflow/switch.py -a -i 1 -n pgf -t 1 -f ipv6_dst=2001:db8:abc:1::0/64 -c fd=pgf.3
+# wifi (route)
+PYTHONPATH=. $TIME -ao $SWLOG python2 openflow/switch.py -a -i 1 -n pgf -t 1 -f ipv6_dst=2001:db8:abc:1::0/64 -c fd=pgf.3
 
-# subnets
+# subnets (routes)
 cnt=4
 for NET in $SUBNETS; do
-    PYTHONPATH=. time -ao $SWLOG python2 openflow/switch.py -a -i 1 -n pgf -f ipv6_dst=2001:db8:abc:$cnt::0/64 -c fd=pgf.$cnt
+    PYTHONPATH=. $TIME -ao $SWLOG python2 openflow/switch.py -a -i 1 -n pgf -f ipv6_dst=2001:db8:abc:$cnt::0/64 -c fd=pgf.$cnt
 
     cnt=$(($cnt+1))
 done
@@ -160,18 +134,18 @@ for HOST in $HOSTS; do
     ADDR=`echo $HOST | cut -d ',' -f 2`
 
     # forwarding rule to host
-    PYTHONPATH=. time -ao $SWLOG python2 openflow/switch.py -a -i 1 -n dmz -t 1 -f ipv6_dst=$ADDR -c fd=dmz.1
+    PYTHONPATH=. $TIME -ao $SWLOG python2 openflow/switch.py -a -i 1 -n dmz -t 1 -f ipv6_dst=$ADDR -c fd=dmz.1
 done
 
 # forwarding rule to firewall (default rule)
-PYTHONPATH=. time -ao $SWLOG python2 openflow/switch.py -a -i 1 -n dmz -t 1 -c fd=dmz.1
+PYTHONPATH=. $TIME -ao $SWLOG python2 openflow/switch.py -a -i 1 -n dmz -t 1 -c fd=dmz.1
 
 # wifi
 # forwarding rule to client
-PYTHONPATH=. time -ao $SWLOG python2 openflow/switch.py -a -i 1 -n wifi -t 1 -f ipv6_dst=2001:db8:abc:1::0/64 -c fd=wifi.2
+PYTHONPATH=. $TIME -ao $SWLOG python2 openflow/switch.py -a -i 1 -n wifi -t 1 -f ipv6_dst=2001:db8:abc:1::0/64 -c fd=wifi.2
 
 # forwarding rule to firewall (default rule)
-PYTHONPATH=. time -ao $SWLOG python2 openflow/switch.py -a -i 1 -n wifi -t 1 -c fd=wifi.1
+PYTHONPATH=. $TIME -ao $SWLOG python2 openflow/switch.py -a -i 1 -n wifi -t 1 -c fd=wifi.1
 
 # subnets
 cnt=4
@@ -184,17 +158,110 @@ for NET in $SUBNETS; do
         PORTS=`echo $HOST | cut -d ',' -f 2-`
 
         # forwarding rule to server
-        PYTHONPATH=. time -ao $SWLOG python2 openflow/switch.py -a -i 1 -n $NET -t 1 -f ipv6_dst=$ADDR -c fd=$NET.$port
+        PYTHONPATH=. $TIME -ao $SWLOG python2 openflow/switch.py -a -i 1 -n $NET -t 1 -f ipv6_dst=$ADDR -c fd=$NET.$port
     done
 
     # forwarding rule to firewall (default rule)
-    PYTHONPATH=. time -ao $SWLOG python2 openflow/switch.py -a -i 1 -n $NET -t 1 -c fd=$NET.1
+    PYTHONPATH=. $TIME -ao $SWLOG python2 openflow/switch.py -a -i 1 -n $NET -t 1 -c fd=$NET.1
 done
 echo "ok"
 
-PYTHONPATH=. python2 netplumber/print_np.py -t
-PYTHONPATH=. python2 netplumber/print_np.py -n
+echo -n "create internet (source)... "
+PYTHONPATH=. $TIME -o $SRCLOG python2 topology/topology.py -at generator -n internet
+PYTHONPATH=. $TIME -o $SRCLLOG python2 topology/topology.py -al internet.1:pgf.1
+echo "ok"
 
-kill -s KILL $AGGR
-kill -s KILL $NP
-#kill -s KILL $RYU
+echo -n "create hosts (source) in dmz... "
+cnt=2
+for  HOST in $HOSTS; do
+    H=`echo $HOST | cut -d ',' -f 1`
+    A=`echo $HOST | cut -d ',' -f 2`
+    P=`echo $HOST | cut -d ',' -f 3-`
+
+    PYTHONPATH=. $TIME -ao $SRCLOG python2 topology/topology.py -a -t generator -n $H -f "ipv6_src=$A"
+    PYTHONPATH=. $TIME -ao $SRCLLOG python2 topology/topology.py -a -l $H.1:dmz.$cnt
+
+    cnt=$(($cnt+1))
+done
+echo "ok"
+
+echo "create hosts (source) in subnets..."
+cnt=4
+for NET in $SUBNETS; do
+    echo -n "  create host $NET... "
+    srv=1
+    for HOST in $SUBHOSTS; do
+        port=$(($srv+1))
+        SRV=source.`echo $HOST | cut -d ',' -f 1`.$NET
+        ADDR=2001:db8:abc:$cnt::$srv
+        PORTS=`echo $HOST | cut -d ',' -f 2-`
+
+        # create source generator
+        PYTHONPATH=. $TIME -ao $SRCLOG python2 topology/topology.py -at generator -n $SRV -f "ipv6_src=$ADDR"
+
+        # link generator to switch
+        PYTHONPATH=. $TIME -ao $SRCLLOG python2 topology/topology.py -al $SRV.1:$NET.$port
+    done
+
+    echo "ok"
+
+    cnt=$(($cnt+1))
+done
+
+
+echo "test ssh reachability from the internet..."
+touch $PROBELOG
+touch $PROBELLOG
+
+echo -n "  test dmz... "
+cnt=2
+for  HOST in $HOSTS; do
+    H=probe.`echo $HOST | cut -d ',' -f 1`
+    A=`echo $HOST | cut -d ',' -f 2`
+    P=`echo $HOST | cut -d ',' -f 3-`
+
+    # add probe that looks for incoming flows for tcp port 22 (ssh) originating from the internet
+    PYTHONPATH=. $TIME -ao $PROBELOG python2 topology/topology.py -a -t probe -n $H -q existential -P ".*(p=pgf.1);$" -F "tcp_dst=22"
+    # link probe to switch
+    PYTHONPATH=. $TIME -ao $PROBELLOG python2 topology/topology.py -a -l $H.1:dmz.$cnt
+
+    # remove probe
+    #PYTHONPATH=. $TIME -ao $PROBELOG python2 topology/topology.py -d -n $H
+    # ... and link
+    #PYTHONPATH=. $TIME -ao $PROBELLOG python2 topology/topology.py -d -l $H.1:dmz.$cnt
+
+    cnt=$(($cnt+1))
+done
+echo "ok"
+
+echo "  test subnets... "
+cnt=4
+for NET in $SUBNETS; do
+    srv=1
+
+    echo -n "    test $NET... "
+
+    for HOST in $SUBHOSTS; do
+        port=$(($srv+1))
+        SRV=probe.`echo $HOST | cut -d ',' -f 1`.$NET
+        ADDR=2001:db8:abc:$cnt::$srv
+        PORTS=`echo $HOST | cut -d ',' -f 2-`
+
+        # add probe that looks for incoming flows for tcp port 22 (ssh) originating from the internet
+        PYTHONPATH=. $TIME -ao $PROBELOG python2 topology/topology.py -a -t probe -n $SRV -q existential -P ".*(p=pgf.1);$" -F "tcp_dst=22"
+        # link probe to switch
+        PYTHONPATH=. $TIME -ao $PROBELLOG python2 topology/topology.py -a -l $NET.$port:$SRV.1
+
+        # remove probe
+        #PYTHONPATH=. $TIME -ao $PROBELOG python2 topology/topology.py -d -n $SRV
+        # ... and link
+        #PYTHONPATH=. $TIME -ao $PROBELLOG python2 topology/topology.py -d -l $NET.$port:$SRV.1
+    done
+
+    echo "ok"
+
+    cnt=$(($cnt+1))
+done
+
+scripts/stop_aggr.sh
+scripts/stop_np.sh
