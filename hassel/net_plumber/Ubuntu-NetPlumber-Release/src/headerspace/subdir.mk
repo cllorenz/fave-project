b00527b274c6319611fbc7ba11c95ca99c7074cc
Claas Lorenz <clorenz@genua.de>
2017-11-24 13:35:20 +0100

    Add titles to svg generation.

--- hassel/net_plumber/Ubuntu-NetPlumber-Release/src/headerspace/subdir.mk
+++ hassel/net_plumber/Ubuntu-NetPlumber-Release/src/headerspace/subdir.mk
@@ -20,7 +20,7 @@ C_DEPS += \
 src/headerspace/%.o: ../src/headerspace/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O3 -Wall -c -fmessage-length=0 -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -O3 -g -Wall -c -fmessage-length=0 -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
