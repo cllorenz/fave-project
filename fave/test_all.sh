76f45cc94b61cc19cb61653f9b85b0e8ab318599
Claas Lorenz <clorenz@genua.de>
2018-02-13 11:52:16 +0100

    Include netplumber unit tests into test suite.

--- fave/test_all.sh
+++ fave/test_all.sh
@@ -9,6 +9,10 @@ mkdir -p $TMPDIR
 echo "start unit tests..."
 PYTHONPATH=. python2 test/unit_tests.py
 
+echo -n "start netplumber tests..."
+net_plumber --test
+echo "ok"
+
 echo -n "regressions... "
 PYTHONPATH=. test/test_rpc.py
 echo "ok"
