210cad6dd05c4a7bd68e624775418437a16119de
Claas Lorenz <clorenz@genua.de>
2018-04-18 19:47:25 +0200

    Only delete np stdout log if existing.

--- fave/scripts/start_np.sh
+++ fave/scripts/start_np.sh
@@ -8,7 +8,7 @@ if [ "$#" -eq "1" ]; then
     net_plumber --log4j-config $1 --hdr-len 1 --unix /tmp/net_plumber.socket >> /tmp/np/stdout.log &
     PID=$!
 else
-    rm -f /tmp/np/stdout.log
+    [ -f /tmp/np/stdout.log ] && rm /tmp/np/stdout.log
     touch /tmp/np/stdout.log
     net_plumber --hdr-len 1 --server 127.0.0.1 1234 >> /tmp/np/stdout.log &
     PID=$!
