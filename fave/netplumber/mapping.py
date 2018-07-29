eaae617602f619ef550b75b951e1dc92a066b48e
Claas Lorenz <clorenz@genua.de>
2018-07-29 18:00:09 +0200

    Lint mapping.

--- fave/netplumber/mapping.py
+++ fave/netplumber/mapping.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python2
 
+""" This module provides data structures for mappings of header fields.
+"""
+
 import json
 #from util.collections_util import diff_dicts
 
@@ -10,13 +13,13 @@ FIELD_SIZES = {
     "packet.ipv6.source" : 128,
     "packet.ipv6.destination" : 128,
     "packet.ipv6.proto" : 8,
-    #"packet.ipv6.priority":, # XXX maybe later
+    #"packet.ipv6.priority":,  # XXX maybe later
     "packet.ipv6.icmpv6.type" : 16,
     "packet.upper.dport" : 16,
     "packet.upper.sport" : 16,
-    #"packet.upper.tcp.flags" : 8, # XXX maybe later
-    #"packet.upper.tcp.syn" : 8, # XXX maybe later
-    #"packet.upper.tcp.option" : , # XXX maybe later
+    #"packet.upper.tcp.flags" : 8,  # XXX maybe later
+    #"packet.upper.tcp.syn" : 8,  # XXX maybe later
+    #"packet.upper.tcp.option" : ,  # XXX maybe later
     "module" : 8,
     "module.limit" : 32,
     "module.state" : 8,
@@ -28,7 +31,7 @@ FIELD_SIZES = {
     "module.ipv6header.rt.len" : 8,
     #"module.ipv6header.rt.0-res" :  # XXX maybe later
     #"module.ipv6header.rt.0-addrs" : # XXX maybe later
-    #"module.ipv6header.rt.0-not-strict" : , # XXX maybe later
+    #"module.ipv6header.rt.0-not-strict" : ,  # XXX maybe later
     "module.ipv6header.ah.spi" : 32,
     "module.ipv6header.ah.len" : 8,
     "module.ipv6header.ah.res" : 16,
@@ -46,77 +49,129 @@ FIELD_SIZES = {
 
 
 class Mapping(dict):
+    """ This class stores mappings of fields giving meaning to bits on a vector.
+    """
 
-    def __init__(self,length=0,mapping={}):
-        super(Mapping,self).__init__(mapping)
+    def __init__(self, length=0, mapping=None):
+        """ Contructs a mapping object.
+
+        Keyword arguments:
+        length -- the total length of the fields chained on a vector
+        mapping -- a predefined mapping stored as a dict
+        """
+
+        if mapping is None:
+            mapping = {}
+
+        super(Mapping, self).__init__(mapping)
         self.length = length
 
+
     def __str__(self):
-        return "length: %i, mapping:\n\t%s" % (
-                self.length,
-                super(dict,self).__str__()
-            )
+        """ Provides a string representation of the mapping.
+        """
+
+        return "length: %i,  mapping:\n\t%s" % (
+            self.length,
+            super(Mapping, self).__str__()
+        )
+
 
     def to_json(self):
+        """ Converts the mapping to JSON.
+        """
+
         tmp = dict(self)
         tmp["length"] = self.length
         return tmp
 
+
     @staticmethod
     def from_json(j):
-        if type(j) == str:
+        """ Constructs a mapping from JSON.
+
+        Keyword arguments:
+        j -- a JSON string or object
+        """
+
+        if isinstance(j, str):
             j = json.loads(j)
 
         length = j["length"]
         del j["length"]
-        return Mapping(length=length,mapping=j)
+        return Mapping(length=length, mapping=j)
 
 
-    def __cmp__(self,other):
-        if type(other) != Mapping:
+    def __cmp__(self, other):
+        """ Compares the mapping with another.
+
+        Keyword arguments:
+        other -- the compared mapping
+        """
+
+        if not isinstance(other, Mapping):
             return False
         if self.length != other.length:
             return False
-        if not super(Mapping,self).__cmp__(super(Mapping,other)):
+        if not super(Mapping, self).__cmp__(super(Mapping, other)):
             return False
         return True
 
-    def extend(self,field):
+
+    def extend(self, field):
+        """ Extends the mapping with a field.
+
+        Keyword arguments:
+        field -- the field identifier to extend the mapping
+        """
+
         self[field] = self.length
         self.length += FIELD_SIZES[field]
 
 
     """
-    def diff(self,other):
+    def diff(self, other):
         if self == other:
             return self
 
-        mapping = diff_dicts(self,other)
+        mapping = diff_dicts(self, other)
 
-        return Mapping(self.length,mapping=mapping)
+        return Mapping(self.length, mapping=mapping)
 
-    def intersect(self,other):
+    def intersect(self, other):
         if self == other
             return self
 
-        mapping = intersect_dicts(self,other)
+        mapping = intersect_dicts(self, other)
 
-        return Mapping(self.length,mapping=mapping)
+        return Mapping(self.length, mapping=mapping)
     """
 
-    def __add__(self,other):
-        mapping = Mapping(length=self.length,mapping=self)
+    def __add__(self, other):
+        """ Adds another mapping.
+
+        Keyword arguments:
+        other -- the added mapping
+        """
+
+        mapping = Mapping(length=self.length, mapping=self)
         mapping.expand(other)
         return mapping
 
 
-    def expand(self,other):
-        assert(type(other) == Mapping)
+    def expand(self, other):
+        """ Expands the mapping by adding fields from another mapping.
+
+        Keyword arguments:
+        other -- the mapping providing fields to be added
+        """
+
+        assert isinstance(other, Mapping)
 
         if self == other:
             return
 
-        uncommon = [k for k in other if not (k in self)]
+        uncommon = [k for k in other if k not in self]
         for k in uncommon:
             self[k] = self.length
             self.length += FIELD_SIZES[k]
