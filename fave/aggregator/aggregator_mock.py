c9171fc439002664c1355a73438bb91ea7505c57
Claas Lorenz <clorenz@genua.de>
2018-08-01 10:36:08 +0200

    Refactoring and linting of the aggregator mock.

--- fave/aggregator/aggregator_mock.py
+++ fave/aggregator/aggregator_mock.py
@@ -1,48 +1,48 @@
 #!/usr/bin/env python2
 
+""" This module offers a mock for the FaVe aggregator.
+"""
+
 import socket
 import os
-import sys
 
-UDS_ADDR = "/tmp/np_aggregator.socket"
+from fave.aggregator.aggregator import UDS_ADDR
 
-class Aggregator(object):
-    BUF_SIZE = 4096
+def main():
+    """ Starts mocking by accepting all incoming FaVe events.
+    """
 
-    def run(self):
-        # open new unix domain socket
-        uds = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
-        uds.bind(UDS_ADDR)
-
-        uds.listen(1)
-
-        while True:
-            # accept connections on unix domain socket
-            try:
-                conn,addr = uds.accept()
-            except socket.error:
-                break
-
-            # receive data from unix domain socket
-            nbytes = Aggregator.BUF_SIZE
-            data = ""
-            while nbytes == Aggregator.BUF_SIZE:
-                tmp = conn.recv(Aggregator.BUF_SIZE)
-                nbytes = len(tmp)
-                data += tmp
-            if not data:
-                break
-
-
-def main(argv):
-    aggregator = Aggregator()
     try:
         os.unlink(UDS_ADDR)
     except OSError:
         if os.path.exists(UDS_ADDR):
             raise
 
-    aggregator.run()
+    buf_size = 4096
+
+    # open new unix domain socket
+    uds = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    uds.bind(UDS_ADDR)
+
+    uds.listen(1)
+
+    while True:
+        # accept connections on unix domain socket
+        try:
+            conn = uds.accept()[0]
+        except socket.error:
+            break
+
+        # receive data from unix domain socket
+        nbytes = buf_size
+        data = ""
+        while nbytes == buf_size:
+            tmp = conn.recv(buf_size)
+            nbytes = len(tmp)
+            data += tmp
+        if not data:
+            break
+
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
