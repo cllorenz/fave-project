3ca9b2b7a752d632f913485b12bd7cc7a8979651
Claas Lorenz <clorenz@genua.de>
2018-08-01 19:11:08 +0200

    Lint probe.

--- fave/topology/probe.py
+++ fave/topology/probe.py
@@ -1,33 +1,38 @@
+""" This module provides a probe model.
+"""
+
 import json
 import itertools
 
-from util.packet_util import ETHER_TYPE_IPV6, normalize_ipv6_address, normalize_ipv6_proto, normalize_upper_port
 from util.path_util import Path
 from util.match_util import OXM_FIELD_TO_MATCH_FIELD
-from netplumber.mapping import Mapping,FIELD_SIZES
+from netplumber.mapping import Mapping, FIELD_SIZES
 from netplumber.vector import set_field_in_vector, Vector, HeaderSpace
 from ip6np.generator import field_value_to_bitvector
 from ip6np.packet_filter import Field
 
 class ProbeModel(object):
-    def __init__(self,node,quantor,filter_expr={},test_expr={}):
+    """ This class provides a probe model.
+    """
+
+    def __init__(self, node, quantor, filter_expr=None, test_expr=None):
         self.node = node
         self.type = "probe"
-        self.ports = { node+'_1' : 1 }
+        self.ports = {node+'_1' : 1}
         self.quantor = quantor
         self.mapping = Mapping()
 
         filter_fields = []
         test_fields = []
 
-        if not filter_expr:
+        if filter_expr is None:
             filter_expr = {'filter_fields':{}}
-        if not test_expr:
-            test_expr = {'test_fields':{},'test_path':[]}
+        if test_expr is None:
+            test_expr = {'test_fields':{}, 'test_path':[]}
 
-        for vectors,fields in [
-            (filter_fields,filter_expr['filter_fields']),
-            (test_fields,test_expr['test_fields'])
+        for vectors, fields in [
+                (filter_fields, filter_expr['filter_fields']),
+                (test_fields, test_expr['test_fields'])
         ]:
             for field in fields:
                 self.mapping.extend(OXM_FIELD_TO_MATCH_FIELD[field])
@@ -37,25 +42,28 @@ class ProbeModel(object):
 
             for comb in combinations:
                 vector = Vector(self.mapping.length)
-                for i,oxm in enumerate(keys):
+                for idx, oxm in enumerate(keys):
                     field = OXM_FIELD_TO_MATCH_FIELD[oxm]
                     set_field_in_vector(
                         self.mapping,
                         vector,
                         field,
                         field_value_to_bitvector(
-                            Field(field,FIELD_SIZES[field],comb[i])
+                            Field(field, FIELD_SIZES[field], comb[idx])
                         ).vector
                     )
                 vectors.append(vector)
 
-        self.filter_fields = HeaderSpace(self.mapping.length,filter_fields)
-        self.test_fields = HeaderSpace(self.mapping.length,test_fields)
+        self.filter_fields = HeaderSpace(self.mapping.length, filter_fields)
+        self.test_fields = HeaderSpace(self.mapping.length, test_fields)
 
         self.test_path = Path(test_expr['test_path'])
 
 
     def to_json(self):
+        """ Converts the probe to JSON.
+        """
+
         return {
             "node" : self.node,
             "type" : self.type,
@@ -68,14 +76,20 @@ class ProbeModel(object):
 
     @staticmethod
     def from_json(j):
-        if type(j) == str:
+        """ Creates a probe from JSON.
+
+        Keyword arguments:
+        j -- a JSON string or object
+        """
+
+        if isinstance(j, str):
             j = json.loads(j)
 
         model = ProbeModel(
             j["node"],
-            j["quantor"],
+            j["quantor"]
         )
-        model.test_path=Path.from_json(j["test_path"])
+        model.test_path = Path.from_json(j["test_path"])
         model.mapping = Mapping.from_json(j["mapping"])
         model.filter_fields = HeaderSpace.from_json(j["filter_fields"])
         model.test_fields = HeaderSpace.from_json(j["test_fields"])
