a00b2a8d964119b06b0dddcf1544045a719d7a74
Claas Lorenz <clorenz@genua.de>
2018-08-01 18:11:09 +0200

    Lint ip6np parser.

--- fave/ip6np/parser.py
+++ fave/ip6np/parser.py
@@ -1,11 +1,17 @@
-from antlr4 import *
+""" This module provides a parser for ip6tables rule sets.
+"""
+
+from antlr4 import InputStream, CommonTokenStream, ParseTreeWalker
+
 from ip6tables_lexer import IP6TablesLexer
 from ip6tables_parser import IP6TablesParser
 from ip6tables_custom_listener import IP6TablesCustomListener
 
-#import time
 
 class ASTParser(object):
+    """ This class offers a parser for ip6tables rule sets.
+    """
+
     _ast = None
 
     @staticmethod
@@ -20,20 +26,18 @@ class ASTParser(object):
 
     @staticmethod
     def parse(ruleset):
-#        t = [time.time()]
+        """ Parses an ip6tables rule set.
+
+        Keyword arguments:
+        ruleset -- an ip6tables rule set as string
+        """
+
         lexer = IP6TablesLexer(InputStream(ruleset))
-#        t.append(time.time())
         tokens = CommonTokenStream(lexer)
-#        t.append(time.time())
         parser = IP6TablesParser(tokens)
-#        t.append(time.time())
         ctx = parser.entry()
-#        t.append(time.time())
         walker = ParseTreeWalker()
-#        t.append(time.time())
         listener = IP6TablesCustomListener()
-#        t.append(time.time())
         walker.walk(listener, ctx)
-#        t.append(time.time())
-#        print "lexer: %s\ntokens: %s\nparser: %s\nctx: %s\nwalker: %s\nlistener: %s\nwalk: %s" % tuple([str(x-t[i]) for i,x in enumerate(t[1:])])
+
         return ASTParser._ast
