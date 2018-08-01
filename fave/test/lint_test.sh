0c2baaff30acba57cccf857bf4c2abf369e6a062
Claas Lorenz <clorenz@genua.de>
2018-08-01 18:52:12 +0200

    Add flow generator linting to script.

--- fave/test/lint_test.sh
+++ fave/test/lint_test.sh
@@ -45,14 +45,14 @@ ip6np/packet_filter.py \
 ip6np/generator.py \
 ip6np/ip6np.py \
 ip6np/parser.py \
-ip6np/ip6tables_custom_listener.py
+ip6np/ip6tables_custom_listener.py \
+topology/generator.py
 "
 
 #./aggregator/aggregator.py
 #./openflow/ofproxy.py
 #./openflow/switch.py
 #./test/check_flows.py
-#./topology/generator.py
 #./topology/host.py
 #./topology/probe.py
 #./wl-ad6-full.py
