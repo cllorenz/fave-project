3e94c9b33eacb440dbdc8a2d639019320f2d7ded
Claas Lorenz <clorenz@genua.de>
2018-08-01 09:08:51 +0200

    Lint print util.

--- fave/util/print_util.py
+++ fave/util/print_util.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python2
 
+""" This module provides printing utilities.
+"""
+
 from __future__ import print_function
 import sys
 
-def eprint(*args,**kwargs):
+def eprint(*args, **kwargs):
+    """ Prints parametrizably to stderr as in Python3
+
+    Keyword arguments:
+    args -- string arguments
+    kwargs -- keywords to customize string output
+    """
     print(*args, file=sys.stderr, **kwargs)
