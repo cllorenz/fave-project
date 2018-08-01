86118e67ec0323d2c20f5c816623cf1836ddc570
Claas Lorenz <clorenz@genua.de>
2018-08-01 09:57:23 +0200

    Lint json utils.

--- fave/util/json_util.py
+++ fave/util/json_util.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python2
 
+""" This module provides utilities to compare JSON objects.
+"""
+
 def _ordered(obj):
-    if isinstance(obj,dict):
-        return sorted({k:_ordered(v) for k,v in obj.items()})
-    elif isinstance(obj,list):
+    if isinstance(obj, dict):
+        return sorted({k:_ordered(v) for k, v in obj.items()})
+    elif isinstance(obj, list):
         return [_ordered(x) for x in obj]
     else:
         return obj
 
-def equal(a,b):
-    return _ordered(a) == _ordered(b)
+def equal(obj1, obj2):
+    """ Checks whether two JSON objects are equal.
+    """
+    return _ordered(obj1) == _ordered(obj2)
