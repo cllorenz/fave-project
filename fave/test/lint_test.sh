af6b40a51bbe1ba9c31160cbe223f3434434bd16
Claas Lorenz <clorenz@genua.de>
2018-08-01 09:22:55 +0200

    Add packet utils linting to script.

--- fave/test/lint_test.sh
+++ fave/test/lint_test.sh
@@ -3,7 +3,7 @@
 TMP=/tmp/pylint.log
 
 #PYFILES=`find . -name "*.py"`
-PYFILES="test/test_rpc.py netplumber/jsonrpc.py __init__.py test/test_netplumber.py netplumber/mapping.py netplumber/vector.py netplumber/model.py test/test_tree.py ip6np/tree.py test/test_topology.py topology/topology.py test/unit_tests.py test/test_utils.py util/print_util.py util/path_util.py"
+PYFILES="test/test_rpc.py netplumber/jsonrpc.py __init__.py test/test_netplumber.py netplumber/mapping.py netplumber/vector.py netplumber/model.py test/test_tree.py ip6np/tree.py test/test_topology.py topology/topology.py test/unit_tests.py test/test_utils.py util/print_util.py util/path_util.py util/packet_util.py"
 
 for PYFILE in $PYFILES; do
     echo -n "lint $PYFILE: "
