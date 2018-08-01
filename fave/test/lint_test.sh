270a18860257fa1aee54ac8d8f5ee7fea452c4e2
Claas Lorenz <clorenz@genua.de>
2018-08-01 10:36:41 +0200

    Add todo list to linting script.

--- fave/test/lint_test.sh
+++ fave/test/lint_test.sh
@@ -3,7 +3,59 @@
 TMP=/tmp/pylint.log
 
 #PYFILES=`find . -name "*.py"`
-PYFILES="test/test_rpc.py netplumber/jsonrpc.py __init__.py test/test_netplumber.py netplumber/mapping.py netplumber/vector.py netplumber/model.py test/test_tree.py ip6np/tree.py test/test_topology.py topology/topology.py test/unit_tests.py test/test_utils.py util/print_util.py util/path_util.py util/packet_util.py util/match_util.py util/json_utils.py"
+PYFILES="\
+test/test_rpc.py \
+netplumber/jsonrpc.py \
+__init__.py \
+test/test_netplumber.py \
+netplumber/mapping.py \
+netplumber/vector.py \
+netplumber/model.py \
+test/test_tree.py \
+ip6np/tree.py \
+test/test_topology.py \
+topology/topology.py \
+test/unit_tests.py \
+test/test_utils.py \
+util/print_util.py \
+util/path_util.py \
+util/packet_util.py \
+util/match_util.py \
+util/json_util.py \
+util/collections_util.py \
+aggregator/aggregator_mock.py
+"
+
+#./aggregator/aggregator.py
+#./aggregator/stop.py
+#./examples/example-traverse.py
+#./ip6np/generator.py
+#./ip6np/ip6np.py
+#./ip6np/ip6tables_custom_listener.py
+#./ip6np/packet_filter.py
+#./netplumber/dump_np.py
+#./netplumber/print_np.py
+#./openflow/ofproxy.py
+#./openflow/switch.py
+#./test/check_flows.py
+#./topology/generator.py
+#./topology/host.py
+#./topology/probe.py
+#./wl-ad6-full.py
+
+
+# to be ignored (generated code):
+#./ip6np/ip6tables_lexer.py
+#./ip6np/ip6tables_listener.py
+#./ip6np/ip6tables_parser.py
+#./ip6np/parser.py
+#./test/antlr/ip6tables_lexer.py
+#./test/antlr/ip6tables_lexer.py
+#./test/antlr/ip6tables_listener.py
+#./test/antlr/ip6tables_parser.py
+#./test/antlr/parser.py
+#./test/antlr/test.py
+
 
 for PYFILE in $PYFILES; do
     echo -n "lint $PYFILE: "
