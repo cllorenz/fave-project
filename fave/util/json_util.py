4406eab88dca7a2ed554dc6e34019c0e91ebaa7c
Claas Lorenz <clorenz@genua.de>
2018-08-02 08:35:18 +0200

    Move prettyfying json to the json utils.

--- fave/util/json_util.py
+++ fave/util/json_util.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python2
 
-""" This module provides utilities to compare JSON objects.
+""" This module provides utilities to handle JSON.
 """
+
+import json
 
 def _ordered(obj):
     if isinstance(obj, dict):
@@ -15,3 +17,16 @@ def equal(obj1, obj2):
     """ Checks whether two JSON objects are equal.
     """
     return _ordered(obj1) == _ordered(obj2)
+
+
+def pstr(j):
+    """ Returns a pretty JSON string.
+
+    Keyword arguments:
+    j -- a JSON string or object
+    """
+
+    if isinstance(j, str):
+        j = json.loads(j)
+
+    return json.dumps(j, sort_keys=True, indent=4, separators=(',', ': '))
