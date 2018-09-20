00a371a263b9a0f39e4030c6e74418804ba870a9
Claas Lorenz <clorenz@genua.de>
2018-09-20 15:17:19 +0200

    Check python return codes in example.

--- fave/examples/example.sh
+++ fave/examples/example.sh
@@ -5,15 +5,15 @@ mkdir -p $TMPDIR
 
 echo -n "clean log files... "
 rm -f $TMPDIR/*.log
-echo "ok"
+[ $? -eq 0 ] && echo "ok" || echo "fail"
 
 echo -n "start netplumber... "
 scripts/start_np.sh examples/example.conf
-echo "ok"
+[ $? -eq 0 ] && echo "ok" || echo "fail"
 
 echo -n "start aggregator... "
 scripts/start_aggr.sh
-echo "ok"
+[ $? -eq 0 ] && echo "ok" || echo "fail"
 
 ##
 ## switch foo
@@ -23,53 +23,79 @@ echo "ok"
 ## probe bla
 ##
 
+PYTHONPATH="${PYTHONPATH}:`pwd`/"
+export PYTHONPATH
+
+CNT=0
+
 # test topology
-echo -n "read topology"
-PYTHONPATH=. python2 topology/topology.py -a -t switch -n foo -p 2
-PYTHONPATH=. python2 topology/topology.py -a -t packet_filter -n bar -i 2001:db8::3 -p 2
-echo -n ", links (foo<->bar)... "
-PYTHONPATH=. python2 topology/topology.py -a -l foo.2:bar.1,bar.3:foo.2
-echo "ok"
+echo -n "read topology... "
+# switch foo
+python2 topology/topology.py -a -t switch -n foo -p 2
+CNT=$(( $? + CNT ))
+# packet filter bar
+python2 topology/topology.py -a -t packet_filter -n bar -i 2001:db8::3 -p 2
+CNT=$(( $? + CNT ))
+# links: foo<->bar
+python2 topology/topology.py -a -l foo.2:bar.1,bar.3:foo.2
+[ $(( $? + CNT )) -eq 0 ] && echo "ok" || echo "fail"
+CNT=0
 
-echo -n "add hosts... generator"
-PYTHONPATH=. python2 topology/topology.py -a -t generator -n baz -f "ipv6_dst=2001:db8::1"
-echo -n ", generator"
-PYTHONPATH=. python2 topology/topology.py -a -t generator -n boz -f "ipv6_dst=2001:db8::2;tcp_dst=80"
+echo -n "add generators... "
+# generator baz
+python2 topology/topology.py -a -t generator -n baz -f "ipv6_dst=2001:db8::1"
+CNT=$(( $? + CNT ))
+# generator boz
+python2 topology/topology.py -a -t generator -n boz -f "ipv6_dst=2001:db8::2;tcp_dst=80"
+CNT=$(( $? + CNT ))
 
-echo -n ", link (baz-->bar)"
-PYTHONPATH=. python2 topology/topology.py -a -l baz.1:bar.2
-echo -n ", link (boz-->foo)"
-PYTHONPATH=. python2 topology/topology.py -a -l boz.1:foo.1
+#link: baz-->bar
+python2 topology/topology.py -a -l baz.1:bar.2
+CNT=$(( $? + CNT ))
+#link: boz-->foo
+python2 topology/topology.py -a -l boz.1:foo.1
+[ $(( $? + CNT )) -eq 0 ] && echo "ok" || echo "fail"
+CNT=0
 
-echo -n ", probe"
-PYTHONPATH=. python2 topology/topology.py -a -t probe -n bla -q universal -P ".*;(table in (bar))"
-echo -n ", probe"
-PYTHONPATH=. python2 topology/topology.py -a -t probe -n blubb -q universal -P ".*;(table in (bar))"
+echo -n "add probes... "
+# probe bla
+python2 topology/topology.py -a -t probe -n bla -q universal -P ".*;(table in (bar))"
+CNT=$(( $? + CNT ))
+python2 topology/topology.py -a -t probe -n blubb -q universal -P ".*;(table in (bar))"
+CNT=$(( $? + CNT ))
 
-echo -n ", link (bar-->bla)"
-PYTHONPATH=. python2 topology/topology.py -a -l bar.4:bla.1
-echo -n ", link (foo-->blubb)... "
-PYTHONPATH=. python2 topology/topology.py -a -l foo.1:blubb.1
-echo "ok"
+# link: bar-->bla
+python2 topology/topology.py -a -l bar.4:bla.1
+CNT=$(( $? + CNT ))
+# link: foo-->blubb
+python2 topology/topology.py -a -l foo.1:blubb.1
+[ $(( $? + CNT )) -eq 0 ] && echo "ok" || echo "fail"
+CNT=0
 
 # test firewall
 echo -n "add firewall rules... "
-#PYTHONPATH=. python2 ip6np/ip6np.py -n bar -i 2001:db8::3 -f ip6np/iptables_ruleset_reduced.sh
-PYTHONPATH=. python2 ip6np/ip6np.py -n bar -i 2001:db8::3 -f rulesets/simple_ruleset.sh
-echo "ok"
+#python2 ip6np/ip6np.py -n bar -i 2001:db8::3 -f ip6np/iptables_ruleset_reduced.sh
+python2 ip6np/ip6np.py -n bar -i 2001:db8::3 -f rulesets/simple_ruleset.sh
+[ $? -eq 0 ] && echo "ok" || echo "fail"
+CNT=0
 
 # test rule setting
 echo -n "add switch rules... "
-PYTHONPATH=. python2 openflow/switch.py -a -i 1 -n foo -t 1 -f ipv6_dst=2001:db8::1 -c fd=foo.1
-PYTHONPATH=. python2 openflow/switch.py -a -i 2 -n foo -t 1 -f ipv6_dst=2001:db8::2 -c fd=foo.2
+python2 openflow/switch.py -a -i 1 -n foo -t 1 -f ipv6_dst=2001:db8::1 -c fd=foo.1
+CNT=$(( $? + CNT ))
+python2 openflow/switch.py -a -i 2 -n foo -t 1 -f ipv6_dst=2001:db8::2 -c fd=foo.2
+CNT=$(( $? + CNT ))
 
-PYTHONPATH=. python2 openflow/switch.py -a -i 1 -n bar -f ipv6_dst=2001:db8::2 -c fd=bar.2
-PYTHONPATH=. python2 openflow/switch.py -a -i 2 -n bar -f ipv6_dst=2001:db8::1 -c fd=bar.1
-echo "ok"
+python2 openflow/switch.py -a -i 1 -n bar -f ipv6_dst=2001:db8::2 -c fd=bar.2
+CNT=$(( $? + CNT ))
 
-PYTHONPATH=. python2 netplumber/dump_np.py -anpft
+python2 openflow/switch.py -a -i 2 -n bar -f ipv6_dst=2001:db8::1 -c fd=bar.1
+[ $(( $? + CNT )) -eq 0 ] && echo "ok" || echo "fail"
+CNT=0
 
-PYTHONPATH=. python2 netplumber/print_np.py -utn
+python2 netplumber/dump_np.py -anpft
+
+python2 netplumber/print_np.py -utn
 
 # test flow propagation
 echo -n "check flow propagation... "
@@ -90,7 +116,8 @@ F5='s=boz && EX t=foo_1 && EX t=bar_pre_routing && EF t=foo_1 && EX p=blubb'
 F6='s=boz && EX t=foo_1 && EX t=bar_pre_routing && EX t=bar_forward_states && EX t=bar_forward_rules'
 F7='s=boz && EX t=foo_1 && EX t=bar_pre_routing && EX t=bar_forward_states'
 
-PYTHONPATH=. python2 test/check_flows.py -c "$F1;$F2;$F3;$F4;$F5;$F6;$F7"
+python2 test/check_flows.py -c "$F1;$F2;$F3;$F4;$F5;$F6;$F7"
+[ $? -eq 0 ] && echo "all example flow tests ok" || echo "some example flow tests failed"
 
 # test openflow
 #echo -n "start ryu... "
