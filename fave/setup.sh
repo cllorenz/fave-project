c6a30006d962bc7b0761cc266961ded82ac77c52
Claas Lorenz <clorenz@genua.de>
2018-09-22 16:13:25 +0200

    Update setup script.

--- fave/setup.sh
+++ fave/setup.sh
@@ -3,4 +3,6 @@
 sudo pacman -S python2-daemon
 sudo pacman -S python2-pip
 sudo pip2 install antlr4-python2-runtime
+sudo pip2 install graphviz
 sudo pacman -S python2-pylint
+sudo pacman -S inkscape
