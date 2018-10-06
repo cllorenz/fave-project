820045b551188d5be89d38717d3610fd1ff17c1e
Claas Lorenz <clorenz@genua.de>
2018-10-06 16:29:54 +0200

    Setup script also for Ubuntu.

--- fave/setup.sh
+++ fave/setup.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-sudo pacman -S python2-daemon
-sudo pacman -S python2-pip
+pacman --version
+if [ $? -eq 0 ]; then
+    sudo pacman -S python2-daemon
+    sudo pacman -S python2-pip
+    sudo pacman -S python2-pylint
+    sudo pacman -S inkscape
+    sudo pacman -S python2-coverage
+fi
+
+apt-get --version
+if [ $? -eq 0 ]; then
+    sudo apt-get install python-daemon
+    sudo apt-get install python-pip
+    sudo apt-get install pylint
+    sudo apt-get install inkscape
+    sudo apt-get install python-coverage
+fi
+
 sudo pip2 install antlr4-python2-runtime
 sudo pip2 install graphviz
-sudo pacman -S python2-pylint
-sudo pacman -S inkscape
-sudo pacman -S python2-coverage
