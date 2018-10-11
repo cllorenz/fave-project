6f7364fda3e73796c9fc4738531e9fdf3cdfc065
Claas Lorenz <clorenz@genua.de>
2018-10-11 20:17:16 +0200

    Fix off by one.

--- fave/ip6np/ip6np.py
+++ fave/ip6np/ip6np.py
@@ -52,7 +52,7 @@ def main(argv):
         elif opt == '-i':
             address = arg
         elif opt == '-p':
-            ports = range(1, int(arg))
+            ports = range(1, int(arg)+1)
         elif opt == '-f':
             ifile = arg
 
