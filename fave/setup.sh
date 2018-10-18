0724773caa8abde82557d32dd23f9e927937cf52
Claas Lorenz <clorenz@genua.de>
2018-10-18 18:14:43 +0200

    Hide OS test output in setup script and add missing dependency.

--- fave/setup.sh
+++ fave/setup.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-pacman --version
+pacman --version 2> /dev/null
 if [ $? -eq 0 ]; then
     sudo pacman -S python2-daemon
     sudo pacman -S python2-pip
     sudo pacman -S python2-pylint
     sudo pacman -S inkscape
     sudo pacman -S python2-coverage
+    sudo ln -s /usr/bin/python2-coverage /usr/bin/coverage2
 fi
 
-apt-get --version
+apt-get --version 2> /dev/null
 if [ $? -eq 0 ]; then
     sudo apt-get install python-daemon
     sudo apt-get install python-pip
