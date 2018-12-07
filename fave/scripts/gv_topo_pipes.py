2207fb97b60dba299f82f2656115672d82ab7d09
Claas Lorenz <clorenz@genua.de>
2018-12-07 14:45:45 +0100

    Lots of linting and code beautifications in graphviz topo script.

--- fave/scripts/gv_topo_pipes.py
+++ fave/scripts/gv_topo_pipes.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python2
 
+""" This module provide functionality to draw a netplumber network.
+"""
+
 import os
 import sys
 import json
 import glob
 import getopt
-import subprocess
 
 from graphviz import Digraph
 
@@ -14,12 +16,25 @@ class MissingData(Exception):
     def __init__(self, message):
         super(MissingData, self).__init__(message)
 
-class topologyRenderer(object):
-    def __init__(self, use_pipes, use_topology, use_policy,
-                 json_tables, json_policy, json_topology, json_pipes, **kwargs):
+
+class TopologyRenderer(object):
+    """ This class builds a graph from a netplumber topology and pipes.
+    """
+
+    def __init__(
+            self,
+            use_pipes,
+            use_topology,
+            use_policy,
+            json_tables,
+            json_policy,
+            json_topology,
+            json_pipes,
+            **kwargs
+        ):
         """
         Class to build a graph of a netplumber topology and pipes
-        
+
         use_pipes: pipes are printed in a subgraph
         use_slice: slices are printed in a subgraph, implies use pipes
         use_topology: topology information is printed in a subgraph separate from pipes
@@ -44,7 +59,7 @@ class topologyRenderer(object):
         self.use_masks = kwargs.get('use_masks', False)
         self.graph = Digraph(
             format=self.format,
-            edge_attr= { 'arrowhead': 'open'}
+            edge_attr={'arrowhead': 'open'}
         )
         self.tgraph = Digraph(name='cluster1')
         self.pgraph = Digraph(name='cluster2')
@@ -63,17 +78,17 @@ class topologyRenderer(object):
 
         self.graph.subgraph(self.fgraph)
         self.graph.subgraph(self.sgraph)
-        self.graph.subgraph(self.pgraph)            
+        self.graph.subgraph(self.pgraph)
         self.graph.subgraph(self.tgraph)
 
     def __build_policy(self):
-        if not (self.use_policy):
+        if not self.use_policy:
             return
-        if not json_policy:
+        if not self.json_policy:
             raise MissingData('Missing Policies')
 
         i = 0
-        for probe in json_policy['commands']:
+        for probe in self.json_policy['commands']:
             source = probe['method'] == 'add_source'
             shape = 'triangle' if source else 'invtriangle'
 
@@ -87,63 +102,71 @@ class topologyRenderer(object):
                     self.tgraph.edge('port'+str(port), 's'+str(i))
 
             i = i+1
-        
+
     def __build_tables(self):
-        if not (self.json_tables):
+        if not self.json_tables:
             raise MissingData('Missing Tables')
 
-        i=0
-        for table in self.json_tables:
-            tgraph = Digraph(name = 'cluster1_' + str(i))
+        table_start = '<TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0">'
+        table_end = '</TABLE>'
+        build_row = lambda x, y: \
+            '<TR><TD align="right">%s</TD><TD>%s</TD></TR>' % (x, y)
+
+        for i, table in enumerate(self.json_tables):
+            tgraph = Digraph(name='cluster1_' + str(i))
             tgraph.attr(rankdir='TD', label='Table '+str(table['id']),
-                        labelloc = 't', labeljust='l')
+                        labelloc='t', labeljust='l')
             for port in table['ports']:
                 tgraph.node('port'+str(port), label=str(port), shape='circle')
 
-            j=0
-            """ TODO(jan): check whether to include rewrite as table row """ 
-            for rule in table['rules']:
-                rewrite = ''
-                if 'rewrite' in rule:
-                    rewrite = '<TR><TD align="right">rewrite:</TD><TD>'+rule['rewrite']+'</TD></TR>'
-                mask = ''
-                if self.use_masks: 
-                    mask = '<TR><TD align="right">mask:</TD><TD>' + rule['mask'] + '</TD></TR>'
-                tgraph.node('rule'+str(i)+str(j), '''<
-                <TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0">
-                <TR><TD align="right">match:</TD><TD>''' + rule['match'] + '''</TD></TR>'''+
-                mask+rewrite+'''
-                </TABLE>>''', shape = 'rectangle')
-                for ip in rule['in_ports']:
-                    tgraph.edge('port'+str(ip), 'rule'+str(i)+str(j))
-                for op in rule['out_ports']:
-                    tgraph.edge('rule'+str(i)+str(j), 'port'+str(op))
-                j=j+1
+            # TODO(jan): check whether to include rewrite as table row
+            for j, rule in enumerate(table['rules']):
+                row_match = build_row('match:', rule['match'])
+
+                row_rewrite = build_row(
+                    'rewrite:', rule['rewrite']
+                ) if 'rewrite' in rule else ''
+
+                row_mask = build_row(
+                    'mask:', rule['mask']
+                ) if 'mask' in rule else ''
+
+                tgraph.node(
+                    'rule'+str(i)+str(j),
+                    "<%s%s%s%s%s>" % (
+                        table_start, row_match, row_mask, row_rewrite, table_end
+                    ),
+                    shape='rectangle'
+                )
+
+                for in_port in rule['in_ports']:
+                    tgraph.edge('port'+str(in_port), 'rule'+str(i)+str(j))
+
+                for out_port in rule['out_ports']:
+                    tgraph.edge('rule'+str(i)+str(j), 'port'+str(out_port))
 
 
             self.tgraph.subgraph(tgraph)
-            i = i+1
 
-            """ action may be:
-                fwd - forward
-                rw  - rewrite
-                encap - encapsulate
-            """
-    
+            # action may be:
+            #    fwd - forward
+            #    rw  - rewrite
+            #    encap - encapsulate
+
     def __build_topology(self):
-        if not (self.use_topology):
+        if not self.use_topology:
             return
-        if not json_topology:
+        if not self.json_topology:
             raise MissingData('Missing Topology')
 
-        for link in json_topology['topology']:
+        for link in self.json_topology['topology']:
             self.tgraph.edge('port'+str(link['src']), 'port'+str(link['dst']))
-        
+
 
     def __build_pipes(self):
-        if not(self.use_pipes):
+        if not self.use_pipes:
             return
-        if not(self.json_pipes):
+        if not self.json_pipes:
             raise MissingData('Missing Pipes')
 
         nodes = set([])
@@ -159,37 +182,45 @@ class topologyRenderer(object):
                     self.pgraph.node(tid, label=str(target), shape='rectangle')
                     nodes.add(tid)
                 self.pgraph.edge(
-                    sid,tid,
-                    color='red:invis:red',style='dashed', penwidth='1')
+                    sid,
+                    tid,
+                    color='red:invis:red',
+                    style='dashed',
+                    penwidth='1'
+                )
 
     def __build_slices(self):
-        if not(self.use_slices):
+        if not self.use_slices:
             return
-        if not(self.json_slices):
+        if not self.json_slices:
             raise MissingData('Missing Slices')
 
         nodes = set([])
-        for slice in self.json_slices['pipes']:
-            start  = slice[0]
+        for np_slice in self.json_slices['pipes']:
+            start = np_slice[0]
             sid = 'slice' + str(start)
             if sid not in nodes:
                 self.sgraph.node(sid, label=str(start), shape='rectangle')
                 nodes.add(sid)
-            for target in slice[1:]:
+            for target in np_slice[1:]:
                 tid = 'slice' + str(target['node_id'])
                 if tid not in nodes:
                     self.sgraph.node(tid, label=str(target['node_id']), shape='rectangle')
                     nodes.add(tid)
-                """ TODO(jan): treat overflow condition """
-                c = '/'+self.colors+'/'+str(target['slice_id']+1)
+                # TODO(jan): treat overflow condition
+                col = '/'+self.colors+'/'+str(target['slice_id']+1)
                 self.sgraph.edge(
-                    sid,tid,
-                    color=c+':invis:'+c,style='dashed', penwidth='1')
+                    sid,
+                    tid,
+                    color=col+':invis:'+col,
+                    style='dashed',
+                    penwidth='1'
+                )
 
     def __build_flows(self):
-        if not(self.use_flows):
+        if not self.use_flows:
             return
-        if not(self.json_flows):
+        if not self.json_flows:
             raise MissingData('Missing Flows')
 
         nodes = set([])
@@ -212,7 +243,7 @@ class topologyRenderer(object):
         self.graph.render(filename, view=False)
 
 def _print_help():
-    print 'usage: python2 ' + os.path.basename(__file__) + ' [-hpst] [-d <dir]'
+    print 'usage: python2 ' + os.path.basename(__file__) + ' [-hfmprst] [-d <dir]'
     print
     print '\t-h this help message'
     print '\t-m includes masks'
@@ -230,8 +261,8 @@ def _read_tables(ddir):
     """
     tables = []
     table_files = glob.glob('%s/*.tf.json' % ddir)
-    for f in table_files:
-        with open(f) as ifile:
+    for table_file in table_files:
+        with open(table_file) as ifile:
             tables = tables + [json.loads(ifile.read())]
     return tables
 
@@ -243,55 +274,55 @@ def _read_files(ddir, name):
 
 if __name__ == '__main__':
     try:
-        opts = getopt.getopt(sys.argv[1:], 'hd:fprst')[0]
+        OPTS, _ARGS = getopt.getopt(sys.argv[1:], 'hd:fmprst')
     except getopt.GetoptError:
         print 'Unable to parse options.'
         sys.exit(1)
 
-    use_dir = 'np_dump'
-    use_masks = False
-    use_pipes = False
-    use_slice = False
-    use_policy = False
-    use_topology = False
-    use_flows = False
+    USE_DIR = 'np_dump'
+    USE_MASKS = False
+    USE_PIPES = False
+    USE_SLICE = False
+    USE_POLICY = False
+    USE_TOPOLOGY = False
+    USE_FLOWS = False
 
-    for opt, arg in opts:
+    for opt, arg in OPTS:
         if opt == '-h':
             _print_help()
             sys.exit(0)
         elif opt == '-m':
-            use_masks = True
+            USE_MASKS = True
         elif opt == '-f':
-            use_flows = True
+            USE_FLOWS = True
         elif opt == '-p':
-            use_policy = True
+            USE_POLICY = True
         elif opt == '-r':
-            use_pipes = True
+            USE_PIPES = True
             """ r wie rohr """
         elif opt == '-s':
-            use_slice = True
+            USE_SLICE = True
         elif opt == '-t':
-            use_topology = True
+            USE_TOPOLOGY = True
         elif opt == '-d':
-            use_dir = arg.rstrip('/')
+            USE_DIR = arg.rstrip('/')
         else:
             print 'No such option: %s.' % opt
             _print_help()
             sys.exit(2)
 
-    """ read required data """
-    json_tables   = _read_tables(use_dir)
-    json_pipes    = _read_files(use_dir, 'pipes')
-    json_policy   = _read_files(use_dir, 'policy')
-    json_topology = _read_files(use_dir, 'topology')
-    json_slices   = _read_files(use_dir, 'slice')
-    json_flows    = _read_files(use_dir, 'flows')
+    # read required data
+    JSON_TABLES = _read_tables(USE_DIR)
+    JSON_PIPES = _read_files(USE_DIR, 'pipes')
+    JSON_POLICY = _read_files(USE_DIR, 'policy')
+    JSON_TOPOLOGY = _read_files(USE_DIR, 'topology')
+    JSON_SLICES = _read_files(USE_DIR, 'slice')
+    JSON_FLOWS = _read_files(USE_DIR, 'flows')
 
-    gb = topologyRenderer(
-        use_pipes, use_topology, use_policy,
-        json_tables, json_policy, json_topology, json_pipes,
-        use_slices=use_slice, json_slices=json_slices,
-        use_flows=use_flows, json_flows=json_flows, use_masks=use_masks)
-    gb.build()
-    gb.render('out')
+    GB = TopologyRenderer(
+        USE_PIPES, USE_TOPOLOGY, USE_POLICY,
+        JSON_TABLES, JSON_POLICY, JSON_TOPOLOGY, JSON_PIPES,
+        use_slices=USE_SLICE, json_slices=JSON_SLICES,
+        use_flows=USE_FLOWS, json_flows=JSON_FLOWS, use_masks=USE_MASKS)
+    GB.build()
+    GB.render('out')
