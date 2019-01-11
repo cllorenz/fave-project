051b658a18d43969b2cb275c005f89affd40fe1d
Claas Lorenz <clorenz@genua.de>
2019-01-11 15:28:43 +0100

    Activate pipe slicing in build.

--- hassel/net_plumber/Ubuntu-NetPlumber-Release/sources.mk
+++ hassel/net_plumber/Ubuntu-NetPlumber-Release/sources.mk
@@ -2,27 +2,27 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-O_SRCS := 
-CPP_SRCS := 
-C_UPPER_SRCS := 
-C_SRCS := 
-S_UPPER_SRCS := 
-OBJ_SRCS := 
-ASM_SRCS := 
-CXX_SRCS := 
-C++_SRCS := 
-CC_SRCS := 
-OBJS := 
-C++_DEPS := 
-C_DEPS := 
-CC_DEPS := 
-CPP_DEPS := 
-EXECUTABLES := 
-CXX_DEPS := 
-C_UPPER_DEPS := 
+O_SRCS :=
+CPP_SRCS :=
+C_UPPER_SRCS :=
+C_SRCS :=
+S_UPPER_SRCS :=
+OBJ_SRCS :=
+ASM_SRCS :=
+CXX_SRCS :=
+C++_SRCS :=
+CC_SRCS :=
+OBJS :=
+C++_DEPS :=
+C_DEPS :=
+CC_DEPS :=
+CPP_DEPS :=
+EXECUTABLES :=
+CXX_DEPS :=
+C_UPPER_DEPS :=
 
-#USER_FLAGS :=-DPIPE_SLICING
-USER_FLAGS :=
+USER_FLAGS :=-DPIPE_SLICING
+#USER_FLAGS :=
 # -fprofile-arcs -ftest-coverage
 DEBUG_FLAGS :=-g
 #-Wextra -Wpedantic
