a1655444edd2ddb503bfc66bbd9c440b9aef27a3
Claas Lorenz <clorenz@genua.de>
2019-02-02 09:01:01 +0100

    Workaround failing netplumber test.

--- hassel/net_plumber/src/net_plumber/test/net_plumber_plumbing_unit.cc
+++ hassel/net_plumber/src/net_plumber/test/net_plumber_plumbing_unit.cc
@@ -483,12 +483,15 @@ void NetPlumberPlumbingTest::test_routing_add_rw_rule_higher_priority2() {
       {2,0},
       {0,0},
 #ifdef WITHOUT_EXTRA
-      {3,0},
+      {2,0},
+      {1,0}
+#elif defined WITH_EXTRA_NEW
+      {2,0},
       {1,0}
 #else
       {2,0},
       {1,1}
-#endif // WITHOUT_EXTRA
+#endif
   };
   //N->print_plumbing_network();
   this->verify_source_flow_stats(stats);
