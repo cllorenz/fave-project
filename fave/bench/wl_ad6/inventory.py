798042ec458385895e987b1296d3e716d59d3bda
Claas Lorenz <clorenz@genua.de>
2019-02-15 08:26:08 +0100

    AD6 wl works with larger subnets again.

--- fave/bench/wl_ad6/inventory.py
+++ fave/bench/wl_ad6/inventory.py
@@ -14,11 +14,10 @@ AD6 = (
         ("vpn", "2001:db8:abc:0::5", [
             "tcp:1194", "tcp:1723", "udp:1194", "udp:1723", "tcp:22", "udp:22"
         ]),
-        # XXX: uncommenting leads to memory explosion
-#        ("dns", "2001:db8:abc:0::6", ["tcp:53", "udp:53", "tcp:22", "udp:22"]),
-#        ("data", "2001:db8:abc:0::7", [
-#            "tcp:118", "tcp:156", "tcp:22", "udp:118", "udp:156", "udp:22"
-#        ]),
+        ("dns", "2001:db8:abc:0::6", ["tcp:53", "udp:53", "tcp:22", "udp:22"]),
+        ("data", "2001:db8:abc:0::7", [
+            "tcp:118", "tcp:156", "tcp:22", "udp:118", "udp:156", "udp:22"
+        ]),
         ("adm", "2001:db8:abc:0::8", ["udp:161", "tcp:22", "udp:22"])
     ],
     [
