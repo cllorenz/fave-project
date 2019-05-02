0422efba6c44daeb91ae852616c9dc2f9b9f4abf
Claas Lorenz <clorenz@genua.de>
2019-05-02 21:53:59 +0200

    Update setup script featuring pybison.

--- fave/setup.sh
+++ fave/setup.sh
@@ -7,6 +7,8 @@ if [ $? -eq 0 ]; then
     sudo pacman -S python2-pylint
     sudo pacman -S inkscape
     sudo pacman -S python2-coverage
+    sudo pacman -S flex
+    sudo pacman -S bison
     sudo ln -s /usr/bin/python2-coverage /usr/bin/coverage2
 fi
 
@@ -17,6 +19,8 @@ if [ $? -eq 0 ]; then
     sudo apt-get install pylint
     sudo apt-get install inkscape
     sudo apt-get install python-coverage
+    sudo apt-get install flex
+    sudo apt-get install bison
 fi
 
 sudo pip2 install antlr4-python2-runtime
@@ -24,3 +28,14 @@ sudo pip2 install graphviz
 sudo pip2 install filelock
 sudo pip2 install pyparsing
 sudo pip2 install pybison
+
+wget http://www.cosc.canterbury.ac.nz/greg.ewing/python/Pyrex/Pyrex-0.9.9.tar.gz
+tar xfz Pyrex-0.9.9.tar.gz
+cd Pyrex-0.9.9
+sudo python2 setup.py install
+cd ..
+
+git clone https://github.com/smvv/pybison.git
+cd pybison
+sudo python2 setup.py install
+cd ..
