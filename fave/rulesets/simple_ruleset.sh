bc2e265391542c7b01199e781017edce41458a84
Claas Lorenz <clorenz@genua.de>
2019-09-15 14:18:00 +0200

    Fix example ruleset.

--- fave/rulesets/simple_ruleset.sh
+++ fave/rulesets/simple_ruleset.sh
@@ -3,5 +3,5 @@ ip6tables -P FORWARD DROP
 ip6tables -P OUTPUT DROP
 
 ip6tables -A FORWARD -d 2001:db8::1 -j ACCEPT
-ip6tables -A FORWARD -d 2001:db8::2 -i 2 -j ACCEPT
+ip6tables -A FORWARD -d 2001:db8::2 -i 1 -j ACCEPT
 
