af53c497e75c81b8d6e0c56dcd47b25a3c299ad0
Claas Lorenz <clorenz@genua.de>
2019-11-20 16:27:18 +0100

    Fix broken masking when counting array wildcards and remove invalid shortcut.

--- hassel/net_plumber/src/headerspace/array.c
+++ hassel/net_plumber/src/headerspace/array.c
@@ -857,10 +857,11 @@ array_merge(const array_t *a, const array_t *b, size_t len) {
         res[i] = isect | diff;
         // count the number of different 0 and 1
         // e.g., 1001 ^ 1000 -> zzzx -> 1
+#ifdef STRICT_RW
+        cnt += x_count(diff, EVEN_MASK);
+#else
         cnt += x_count(diff, ODD_MASK);
-        // XXX: shortcut possibly leads to jump based on unitialized value
-        //      in array_has_z()
-        //if (cnt > 1) break;
+#endif
     }
 
     if (array_has_z(res, len)) return NULL;
