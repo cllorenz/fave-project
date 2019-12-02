e3d1fe91c6e494db2b8957df2ee3a2254180e833
Claas Lorenz <clorenz@genua.de>
2019-12-02 17:11:45 +0100

    Instrument probes to check end-to-end reachability correctly.

--- fave/bench/wl_ifi/policygen.py
+++ fave/bench/wl_ifi/policygen.py
@@ -8,28 +8,37 @@ import json
 from bench.wl_ifi.inventory import SUBNETS
 
 IFILE="bench/wl_ifi/inventory.json"
+REACH_JSON="bench/wl_ifi/reachable.json"
 OFILE="bench/wl_ifi/policies.json"
 
 if __name__ == '__main__':
     with open(IFILE, 'r') as inv_file:
         inventory = json.load(inv_file)
 
+    with open(REACH_JSON, 'r') as reach_file:
+        reachable = json.load(reach_file)
+
     domain_to_ports = inventory["domain_to_ports"]
 
     probes = [
-        ("probe.Internet", "probe", "universal", None, None, ["(p in (ifi.18))"]),
-        ("probe.external.ifi", "probe", "universal", None, None, [".*(p=ifi.19);$"])
+        ("probe.Internet", "probe", "universal", None, None, [
+            ".*(p in (admin.ifi.2,external.ifi.2,lab.ifi.2,office.ifi.2,pool.ifi.2,staff_1.ifi.2,staff_2.ifi.2))$"
+        ]),
+        ("probe.external.ifi", "probe", "universal", None, None, [
+            ".*(p in (ifi.1,admin.ifi.2,external.ifi.2,lab.ifi.2,office.ifi.2,pool.ifi.2,slb.ifi.2,staff_1.ifi.2,staff_2.ifi.2))$"
+        ])
     ]
 
-    out_port = lambda _ip, op: op
     probes.extend([
         (
             "probe.%s" % sub,
             "probe",
-            "existential",
+            "universal",
             None,
             None,
-            [".*(p=ifi.%s);$" % out_port(*domain_to_ports[sub])]
+            [".*(p in (%s))$" % ','.join(
+                [("%s.2" % s) if s != "Internet" else "ifi.1" for s in reachable[sub]]
+            )]
         ) for sub in SUBNETS
     ])
 
