84bb4d890e5c3c76d3327fbff975cc205ea53f39
Claas Lorenz <clorenz@genua.de>
2019-12-18 16:51:36 +0100

    Fix another typo.

--- fave/examples/example.sh
+++ fave/examples/example.sh
@@ -62,7 +62,7 @@ python2 topology/topology.py -a -l $HOST2.1:$SWITCH.1
 [ $(( $? + CNT )) -eq 0 ] && echo "ok" || echo "fail"
 CNT=0
 
-echo -n "add probess... "
+echo -n "add probes... "
 # PROBE1 $PROBE1
 python2 topology/topology.py -a -t probe -n $PROBE1 -q universal -P ".*;(table in ($FIREWALL))"
 CNT=$(( $? + CNT ))
