4510a8e61bbbbc05a0384f0349308bba60f82d38
Claas Lorenz <clorenz@genua.de>
2020-01-01 10:27:00 +0100

    Fix warning when compiling C code with C++ std compiler option.

--- hassel/net_plumber/Ubuntu-NetPlumber-Release/src/net_plumber/subdir.mk
+++ hassel/net_plumber/Ubuntu-NetPlumber-Release/src/net_plumber/subdir.mk
@@ -50,7 +50,7 @@ CC_DEPS += \
 src/net_plumber/%.o: ../src/net_plumber/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -DJSON_IS_AMALGAMATION -I/usr/include/ $(GCFLAGS) -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -DJSON_IS_AMALGAMATION -I/usr/include/ $(GCFLAGS) -std=c++14 -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
