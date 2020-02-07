38aaa63cfd9aa542f5127e7d990975156dcf0aa8
Claas Lorenz <cllorenz@uni-potsdam.de>
2020-02-07 19:21:43 +0100

    Deactivate icpmv6 and udp rules in host ruleset generation.

--- fave/scripts/generate-host-rulesets.sh
+++ fave/scripts/generate-host-rulesets.sh
@@ -28,15 +28,24 @@ function private {
 }
 
 
-DMZ="file.uni-potsdam.de,2001:db8:abc:1::1,tcp:21,tcp:115;tcp:22,udp:22 \
+DMZ="file.uni-potsdam.de,2001:db8:abc:1::1,tcp:21,tcp:115;tcp:22 \
     mail.uni-potsdam.de,2001:db8:abc:1::2,tcp:25,tcp:587,tcp:110,tcp:143,tcp:220,tcp:465,\
-tcp:993,tcp:995,udp:143,udp:220;tcp:22,udp:22 \
-    web.uni-potsdam.de,2001:db8:abc:1::3,tcp:80,tcp:443;tcp:22,udp:22 \
-    ldap.uni-potsdam.de,2001:db8:abc:1::4,tcp:389,tcp:636,udp:389,udp:123;tcp:22,udp:22 \
-    vpn.uni-potsdam.de,2001:db8:abc:1::5,tcp:1194,tcp:1723,udp:1194,udp:1723;tcp:22,udp:22 \
-    dns.uni-potsdam.de,2001:db8:abc:1::6,tcp:53,udp:53;tcp:22,udp:22 \
-    data.uni-potsdam.de,2001:db8:abc:1::7,;tcp:118,tcp:156,tcp:22,udp:118,udp:156,udp:22 \
-    adm.uni-potsdam.de,2001:db8:abc:1::8,;udp:161,tcp:22,udp:22"
+tcp:993,tcp:995;tcp:22 \
+    web.uni-potsdam.de,2001:db8:abc:1::3,tcp:80,tcp:443;tcp:22 \
+    ldap.uni-potsdam.de,2001:db8:abc:1::4,tcp:389,tcp:636;tcp:22 \
+    vpn.uni-potsdam.de,2001:db8:abc:1::5,tcp:1194,tcp:1723;tcp:22 \
+    dns.uni-potsdam.de,2001:db8:abc:1::6,tcp:53,udp:53;tcp:22 \
+    data.uni-potsdam.de,2001:db8:abc:1::7,;tcp:118,tcp:156,tcp:22 \
+    adm.uni-potsdam.de,2001:db8:abc:1::8,;udp:161,tcp:22"
+#DMZ="file.uni-potsdam.de,2001:db8:abc:1::1,tcp:21,tcp:115;tcp:22,udp:22 \
+#    mail.uni-potsdam.de,2001:db8:abc:1::2,tcp:25,tcp:587,tcp:110,tcp:143,tcp:220,tcp:465,\
+#tcp:993,tcp:995,udp:143,udp:220;tcp:22,udp:22 \
+#    web.uni-potsdam.de,2001:db8:abc:1::3,tcp:80,tcp:443;tcp:22,udp:22 \
+#    ldap.uni-potsdam.de,2001:db8:abc:1::4,tcp:389,tcp:636,udp:389,udp:123;tcp:22,udp:22 \
+#    vpn.uni-potsdam.de,2001:db8:abc:1::5,tcp:1194,tcp:1723,udp:1194,udp:1723;tcp:22,udp:22 \
+#    dns.uni-potsdam.de,2001:db8:abc:1::6,tcp:53,udp:53;tcp:22,udp:22 \
+#    data.uni-potsdam.de,2001:db8:abc:1::7,;tcp:118,tcp:156,tcp:22,udp:118,udp:156,udp:22 \
+#    adm.uni-potsdam.de,2001:db8:abc:1::8,;udp:161,tcp:22,udp:22"
 
 SUBNETS="api.uni-potsdam.de \
     asta.uni-potsdam.de \
@@ -60,12 +69,17 @@ SUBNETS="api.uni-potsdam.de \
     ub.uni-potsdam.de \
     welcome-center-potsdam.de"
 
-SUBHOSTS="web,tcp:80,tcp:443;tcp:22,udp:22 \
-    voip,tcp:5060,tcp:5061,udp:5060;tcp:22,udp:22 \
-    print,;tcp:631,tcp:22,udp:631,udp:22 \
-    mail,tcp:25,tcp:587,tcp:110,tcp:143,tcp:220,tcp:465,tcp:993,tcp:995;tcp:22,\
-udp:143,udp:220,udp:22 \
-    file,;tcp:137,tcp:138,tcp:139,tcp:445,tcp:2049,tcp:22,udp:137,udp:138,udp:139,udp:22"
+SUBHOSTS="web,tcp:80,tcp:443;tcp:22 \
+    voip,tcp:5060,tcp:5061;tcp:22 \
+    print,;tcp:631,tcp:22 \
+    mail,tcp:25,tcp:587,tcp:110,tcp:143,tcp:220,tcp:465,tcp:993,tcp:995;tcp:22 \
+    file,;tcp:137,tcp:138,tcp:139,tcp:445,tcp:2049,tcp:22"
+#SUBHOSTS="web,tcp:80,tcp:443;tcp:22,udp:22 \
+#    voip,tcp:5060,tcp:5061,udp:5060;tcp:22,udp:22 \
+#    print,;tcp:631,tcp:22,udp:631,udp:22 \
+#    mail,tcp:25,tcp:587,tcp:110,tcp:143,tcp:220,tcp:465,tcp:993,tcp:995;tcp:22,\
+#udp:143,udp:220,udp:22 \
+#    file,;tcp:137,tcp:138,tcp:139,tcp:445,tcp:2049,tcp:22,udp:137,udp:138,udp:139,udp:22"
 
 
 
@@ -84,14 +98,14 @@ for HOST in $DMZ; do
     echo "ip6tables -P OUTPUT ACCEPT" >> $SCRIPT
 
     # handle incoming icmpv6
-    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type destination-unreachable -j ACCEPT" >> $SCRIPT
-    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type packet-too-big -j ACCEPT" >> $SCRIPT
-    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type time-exceeded -j ACCEPT" >> $SCRIPT
-    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type parameter-problem -j ACCEPT" >> $SCRIPT
-    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-request -m limit --limit 900/min -j ACCEPT" >> $SCRIPT
-    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-reply -m limit --limit 900/min -j ACCEPT" >> $SCRIPT
-    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbour-solicitation -j ACCEPT" >> $SCRIPT
-    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbour-advertisement -j ACCEPT" >> $SCRIPT
+#    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type destination-unreachable -j ACCEPT" >> $SCRIPT
+#    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type packet-too-big -j ACCEPT" >> $SCRIPT
+#    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type time-exceeded -j ACCEPT" >> $SCRIPT
+#    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type parameter-problem -j ACCEPT" >> $SCRIPT
+#    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-request -m limit --limit 900/min -j ACCEPT" >> $SCRIPT
+#    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-reply -m limit --limit 900/min -j ACCEPT" >> $SCRIPT
+#    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbour-solicitation -j ACCEPT" >> $SCRIPT
+#    echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbour-advertisement -j ACCEPT" >> $SCRIPT
 
     # accept traffic for public services
     for PORT in $PUB; do
@@ -122,14 +136,14 @@ for SUB in $SUBNETS; do
         echo "ip6tables -P OUTPUT ACCEPT" >> $SCRIPT
 
         # handle incoming icmpv6
-        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type destination-unreachable -j ACCEPT" >> $SCRIPT
-        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type packet-too-big -j ACCEPT" >> $SCRIPT
-        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type time-exceeded -j ACCEPT" >> $SCRIPT
-        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type parameter-problem -j ACCEPT" >> $SCRIPT
-        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-request -m limit --limit 900/min -j ACCEPT" >> $SCRIPT
-        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-reply -m limit --limit 900/min -j ACCEPT" >> $SCRIPT
-        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbour-solicitation -j ACCEPT" >> $SCRIPT
-        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbour-advertisement -j ACCEPT" >> $SCRIPT
+#        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type destination-unreachable -j ACCEPT" >> $SCRIPT
+#        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type packet-too-big -j ACCEPT" >> $SCRIPT
+#        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type time-exceeded -j ACCEPT" >> $SCRIPT
+#        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type parameter-problem -j ACCEPT" >> $SCRIPT
+#        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-request -m limit --limit 900/min -j ACCEPT" >> $SCRIPT
+#        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-reply -m limit --limit 900/min -j ACCEPT" >> $SCRIPT
+#        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbour-solicitation -j ACCEPT" >> $SCRIPT
+#        echo "ip6tables -A INPUT -p icmpv6 --icmpv6-type neighbour-advertisement -j ACCEPT" >> $SCRIPT
 
         # accept traffic for public services
         for PORT in $PUB; do
