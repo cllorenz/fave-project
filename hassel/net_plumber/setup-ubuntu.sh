834a86d29354452d9983154df109a9827777b3d9
Claas Lorenz <cllorenz@uni-potsdam.de>
2020-03-19 15:39:33 +0100

    Include bdd library in setup script.

--- hassel/net_plumber/setup-ubuntu.sh
+++ hassel/net_plumber/setup-ubuntu.sh
@@ -3,3 +3,10 @@ sudo apt-get install liblog4cxx10v5
 sudo apt-get install liblog4cxx-dev
 sudo apt-get install libcppunit-1.14-0
 sudo apt-get install libcppunit-dev
+
+git clone https://github.com/jgcoded/BuDDy.git
+cd BuDDy
+sh configure
+make
+sudo make install
+cd ..
