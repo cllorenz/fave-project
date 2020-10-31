4079ee0dc5f3579c72cb7b93debe3382372e96f3
Claas Lorenz <clorenz@genua.de>
2020-10-31 19:19:14 +0100

    Update license information.

--- net_plumber/src/headerspace/test/hs_unit.h
+++ net_plumber/src/headerspace/test/hs_unit.h
@@ -1,19 +1,8 @@
 /*
-   Copyright 2018 Claas Lorenz
+  Copyright 2018, Claas Lorenz. This file is licensed under GPL v2 plus
+  a special exception, as described in included LICENSE_EXCEPTION.txt.
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-   Author: cllorenz@uni-potsdam.de (Claas Lorenz)
+  Author: cllorenz@uni-potsdam.de (Claas Lorenz)
 */
 
 #ifndef SRC_NET_PLUMBER_TEST_HS_UNIT_H_
