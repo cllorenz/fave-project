8915cfe680f1c6cff40d1090255ea651a95c7674
Claas Lorenz <clorenz@genua.de>
2020-12-06 14:27:02 +0100

    New FPL attributes for IPv4 and IPv6 gateway attributes.

--- policy-translator/policy.py
+++ policy-translator/policy.py
@@ -457,7 +457,7 @@ class Role(object):
             objects as values.
     """
 
-    valid_role_attr = ["description", "hosts", "vlan", "ipv4", "ipv6", "gateway"]
+    valid_role_attr = ["description", "hosts", "vlan", "ipv4", "ipv6", "gateway", "gateway4", "gateway6"]
 
     def __init__(self, name, policy, attributes=None, services=None):
         """Initialises a Role object with the given name, policy, attributes and
