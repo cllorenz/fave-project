98617ba32910a93ada6ef34cf18852f4b885c7db
Claas Lorenz <clorenz@genua.de>
2021-01-04 13:06:04 +0100

    Enforce multiples of 8 for length expansions.

--- net_plumber/src/net_plumber/rpc_handler.cc
+++ net_plumber/src/net_plumber/rpc_handler.cc
@@ -432,6 +432,7 @@ PROTO(reset_plumbing_network)
  */
 PROTO(expand)
   size_t len = PARAM(length).asUInt64();
+  assert (len % 8 == 0);
   size_t ret = netPlumber->expand((len / 8) + ((len % 8) ? 1 : 0));
   length = netPlumber->get_length();
   RETURN((Json::Value::UInt64) ret);
