27774ed7795150633e439d877eb39daa6efe2767
Claas Lorenz <clorenz@genua.de>
2021-01-04 18:23:24 +0100

    Remove mapping and private ports from model.

--- fave/netplumber/model.py
+++ fave/netplumber/model.py
@@ -31,8 +31,8 @@ class Model(object):
     """ This class stores a basic model for a node.
     """
 
-    def __init__(self, node, mtype="model", tables=None, ports=None, wiring=None, mapping=None):
-        """ Constructs a basic NetPlumber model
+    def __init__(self, node, mtype="model", tables=None, ports=None, wiring=None):
+        """ Constructs a basic NetPlumber model.
 
         Keyword arguments:
         node -- the node's name
@@ -40,26 +40,22 @@ class Model(object):
         tables -- a list of tables storing rules
         ports -- a mapping of port lists to the model's tables
         wiring -- a list of unidirectional links between the model's ports
-        mapping -- a mapping for the rule vectors
         """
 
         self.node = node
         self.type = mtype
         self.tables = tables if tables is not None else {}
         self.ports = ports if ports is not None else {}
-        self.private_ports = 0
         self.wiring = wiring if wiring is not None else []
-        self.mapping = mapping if mapping is not None else Mapping()
 
 
     def __str__(self):
-        return "node: %s\ntype: %s\ntables:\n\t%s\nports:\n\t%s\nwiring:\n\t%s\nmapping:\n\t%s" % (
+        return "node: %s\ntype: %s\ntables:\n\t%s\nports:\n\t%s\nwiring:\n\t%s" % (
             self.node,
             self.type,
             self.tables,
             self.ports,
-            self.wiring,
-            self.mapping
+            self.wiring
         )
 
 
@@ -71,7 +67,6 @@ class Model(object):
         """
         assert isinstance(mapping, Mapping)
 
-        self.mapping.expand(mapping)
 
         for table in self.tables:
             for rule in self.tables[table]:
@@ -91,9 +86,7 @@ class Model(object):
                 ]  for tk, t in self.tables.iteritems()
             },
             "ports" : self.ports,
-            "wiring" : self.wiring,
-            "mapping" : self.mapping.to_json(),
-            "private_ports" : self.private_ports
+            "wiring" : self.wiring
         }
 
 
@@ -130,10 +123,8 @@ class Model(object):
             j["type"],
             tables=j["tables"],
             ports=j["ports"],
-            wiring=[(p1, p2) for p1, p2 in j["wiring"]],
-            mapping=Mapping(j["mapping"])
+            wiring=[(p1, p2) for p1, p2 in j["wiring"]]
         )
-        model.private_ports = j.get("private_ports", 0)
 
         return model
 
@@ -150,15 +141,13 @@ class Model(object):
 
         ports = dict_sub(self.ports, other.ports)
         wiring = list_sub(self.wiring, other.wiring)
-        mapping = self.mapping + other.mapping
 
         return Model(
             self.node,
             mtype=self.type,
             tables=tables,
             ports=ports,
-            wiring=wiring,
-            mapping=mapping
+            wiring=wiring
         )
 
 
@@ -171,7 +160,6 @@ class Model(object):
             self.tables == other.tables,
             self.ports == other.ports,
             self.wiring == other.wiring,
-            self.mapping == other.mapping
         ])
 
     def __ne__(self, other):
