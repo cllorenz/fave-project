332ff68f5e79fc493e0da54a3d60484f2ea37471
Claas Lorenz <clorenz@genua.de>
2021-01-20 21:58:52 +0100

    Minor benchmark simplification.

--- fave/bench/wl_tum/routegen.py
+++ fave/bench/wl_tum/routegen.py
@@ -8,10 +8,7 @@ import json
 OFILE="bench/wl_tum/routes.json"
 
 if __name__ == '__main__':
-    routes = [
-        # default route to the Internet
-        ("fw.tum", 1, 65535, [], ["fd=fw.tum.1"], [])
-    ]
+    routes = []
 
     with open(OFILE, 'w') as of:
         of.write(json.dumps(routes, indent=2) + "\n")
