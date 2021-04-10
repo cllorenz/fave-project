634ede90f263f28e6412bb198f7220c338295d93
Claas Lorenz <clorenz@genua.de>
2021-04-10 11:57:14 +0200

    Minor logging change.

--- fave/aggregator/aggregator.py
+++ fave/aggregator/aggregator.py
@@ -119,7 +119,7 @@ class Aggregator(AbstractAggregator):
             try:
                 j = json.loads(data)
             except ValueError:
-                Aggregator.LOGGER.exception('worker: could not parse data: %s' % data)
+                Aggregator.LOGGER.fatal('worker: could not parse data: %s' % data)
                 self.queue.task_done()
                 return
 
