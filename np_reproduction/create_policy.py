2b1deb5fbcc57af0ca3a526e60a03fb94c621453
Claas Lorenz <clorenz@genua.de>
2021-05-16 17:58:53 +0200

    Fix policy file generation.

--- np_reproduction/create_policy.py
+++ np_reproduction/create_policy.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python2
 
 import json
-import random
-random.seed(1)
 import sys
 
 src_dir = sys.argv[1]
 dst_dir = src_dir
 
 config_json = json.load(open("%s/config.json" % src_dir))
+with_paths = config_json.get("with_paths", False)
 tables = config_json["tables"]
 hdr_len = config_json["length"]
 
 commands = []
 
 table_out_json = {
-#    table : json.load(open("%s/%s.tf.json" % (src_dir, table))) for table in tables
     table : json.load(open("%s/%s.out.rules.json" % (src_dir, table))) for table in tables
 }
 
@@ -33,38 +31,9 @@ table_in_ports = {
     table : [p for p in table_in_json[table]['ports'] if p != tid*100000] for tid, table in enumerate(tables, start=1)
 }
 
-table_used_in_ports = {}
-for table, json_table in table_in_json.iteritems():
-    used_in_ports = set()
-    for rule in json_table['rules']:
-        used_in_ports.update(set(rule['in_ports']))
-
-    table_used_in_ports[table] = used_in_ports
-
-topology_used_in_ports = set(
-    [l['dst'] for l in topology_json['topology']]
-)
-
-random_in_port = {
-#    table : random.choice(table_in_ports[table]) for table in tables
-    table : random.choice(list(table_used_in_ports[table] - topology_used_in_ports)) for table in tables
-}
-
 first_in_port = {
     table : table_in_ports[table][0] for table in tables
 }
-
-table_used_out_ports = {}
-for table, json_table in table_out_json.iteritems():
-    used_out_ports = set()
-    for rule in json_table['rules']:
-        used_out_ports.update(set(rule['out_ports']))
-
-    table_used_out_ports[table] = used_out_ports
-
-topology_used_out_ports = set(
-    [l['src'] for l in topology_json['topology']]
-)
 
 probes = {
     table : [
@@ -82,7 +51,7 @@ for tid, table in enumerate(tables, start=1):
     table_probes = probes[table]
 
     for table, table_probe in zip(tables, table_probes):
-#        _sid, source_port = sources[table]
+        _sid, source_port = sources[table]
         source_port = first_in_port[table]
         pid, probe_port = table_probe
         source_commands.append({
@@ -91,24 +60,23 @@ for tid, table in enumerate(tables, start=1):
                 "filter" : { "type" : "true" },
 				"match" : ','.join(["xxxxxxxx"]*hdr_len),
 				"ports" : [ probe_port ],
-				"test" :
+				"test" : # complex expression if paths should be checked
 				{
-                    "type" : "true"
-#					"type" : "path",
-#                    "pathlets" : [
-#                        {
-#                            "type" : "last_ports",
-#                            "ports" : [ source_port ]
-#                        }
-#                    ]
-				},
+					"type" : "path",
+                    "pathlets" : [
+                        {
+                            "type" : "last_ports",
+                            "ports" : [ source_port ]
+                        }
+                    ]
+				} if with_paths else { "type" : "true" },
                 "mode" : "existential",
                 "id" : pid
             },
         })
 
         # connect all egress ports to probe
-        for table_port in table_out_ports[table]: #(table_used_out_ports[table] - topology_used_out_ports):
+        for table_port in table_out_ports[table]:
             source_commands.append({
                 "method" : "add_link",
                 "params" : {
@@ -116,6 +84,9 @@ for tid, table in enumerate(tables, start=1):
                     "to_port" : probe_port
                 }
             })
+
+        # skip further probes if just the simple check is used
+        if not with_paths: break
 
 
 # add and connect sources
@@ -136,9 +107,8 @@ for tid, table in enumerate(tables, start=1):
 		},
 	})
 
-    # connect source to one random ingress port
-    table_port = first_in_port[table] #random_in_port[table] #random.choice(table_ports)
-#    for table_port in table_ports:
+    # connect source to first ingress port
+    table_port = first_in_port[table]
     commands.append({
         "method" : "add_link",
         "params" : {
