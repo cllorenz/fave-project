bb6c2516ead102164d860e2b4d6500e60a5297b0
Claas Lorenz <cllorenz@uni-potsdam.de>
2021-05-24 09:29:21 +0200

    Some minor refactorings.

--- np_reproduction/transform.py
+++ np_reproduction/transform.py
@@ -11,13 +11,10 @@ def toggle_mask_bits(mask):
     return mask.replace('0', 'x').replace('1', '0').replace('x', '1') if mask else mask
 
 config_json = json.load(open("%s/config.json" % src_dir))
-tables = {table : idx*10 for idx, table in enumerate(config_json["tables"], start=1)}
 
-ttypes = { ttype : idx for idx, ttype in enumerate(config_json["table_types"]) }
-
-for tid, table in enumerate(tables, start=1):
+for tid, table in enumerate(config_json['tables'], start=1):
     print "table", table
-    for ttid, ttype in enumerate(ttypes):
+    for ttid, ttype in enumerate(config_json['table_types']):
         etid = tid * 10 + ttid
         print "  ttype", ttype
         print "    read from", '%s/%s.tf.json' % (src_dir, etid)
@@ -31,8 +28,8 @@ for tid, table in enumerate(tables, start=1):
             rule['id'] = (etid << 32) + rid + 1
             rule['mask'] = toggle_mask_bits(rule['mask'])
 
-        print "    write to %s/%s.tf.json" % (dst_dir, tid * 10 + ttid)
-        json.dump(tab, open('%s/%s.tf.json' % (dst_dir, tid * 10 + ttid), 'w'), indent=1)
+        print "    write to %s/%s.tf.json" % (dst_dir, etid)
+        json.dump(tab, open('%s/%s.tf.json' % (dst_dir, etid), 'w'), indent=1)
 
 os.system('cp %s/config.json %s/config.json' % (src_dir, dst_dir))
 os.system('cp %s/topology.json %s/topology.json' % (src_dir, dst_dir))
