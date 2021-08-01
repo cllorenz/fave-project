2e07ce95a23716bd9459204122c77436bec977c6
Claas Lorenz <clorenz@genua.de>
2021-08-01 16:48:09 +0200

    Clean up main script.

--- ad6/main.sh
+++ ad6/main.sh
@@ -5,8 +5,6 @@ check() {
 	echo "ps -e | grep $PID: $RUNS"
 }
 
-bash scripts/startdb.sh
-
 #python3 src/generator/generator.py
 if [ -f time.log ]; then
 	rm time.log
@@ -29,31 +27,4 @@ if [ -f post.mem.log ]; then
 fi
 
 TTY=$(tty)
-time PYTHONPATH=. python3 main.py | tee $TTY > /tmp/main.log
-PID=$!
-
-killall redis-server
-
-exit 0
-
-echo "python PID: $PID"
-ps -o size,rssize,vsize -p $PID > memory.log
-
-#paste pre.mem.log in.mem.log post.mem.log | sed 1d > memory.log
-
-#exit 0
-
-check
-#RUNS=""
-while [ "$RUNS" != "" ]; do
-	ps -o size,rssize,vsize -p $PID | sed 1d >> memory.log
-	sleep 0.2
-	check
-done
-
-echo "break"
-
-
-#python3 testall.py
-
-#python3 service.py
+time PYTHONPATH=. python3 main.py | tee $TTY > ./main.log
