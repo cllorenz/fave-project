6aecbaeb96b96e035527d563b1714e07b72eca18
Claas Lorenz <clorenz@genua.de>
2021-08-07 08:38:15 +0200

    Refactor common packet filter utilities into own abstract class.

--- fave/ip6np/packet_filter.py
+++ fave/ip6np/packet_filter.py
@@ -40,106 +40,24 @@ BASE_ROUTING_WRONG_AP=65535/4*2
 BASE_ROUTING_RULE=65535/4*3
 
 
-class PacketFilterModel(Model):
-    """ This class stores packet filter models.
-    """
+class AbstractPacketFilterModel(Model):
+    def __init__(self, node, pf_type, ports=None):
+        super(AbstractPacketFilterModel, self).__init__(node, pf_type)
 
-    def __init__(self, node, ports=None, address=None):
-        super(PacketFilterModel, self).__init__(node, "packet_filter")
-
-        ports = ports if ports is not None else ["1", "2"]
-
-        self.tables = {
-            node + ".pre_routing" : [],
-            node + ".input_filter" : [],
-            node + ".output_filter" : [],
-            node + ".forward_filter" : [],
-            node + ".routing" : [],
-            node + ".post_routing" : [],
-            node + ".internals" : [],
+        self.ports = {
+            node + '.' + str(port) : "" for port in (
+                ports if ports is not None else ["1", "2"]
+            )
         }
-
-        self.internal_ports = {
-            node + ".pre_routing_input" : node + ".pre_routing",
-            node + ".pre_routing_forward" : node + ".pre_routing",
-            node + ".input_filter_in" : node + ".input_filter",
-            node + ".input_filter_accept" : node + ".input_filter",
-            node + ".forward_filter_in" : node + ".forward_filter",
-            node + ".forward_filter_accept" : node + ".forward_filter",
-            node + ".output_filter_in" : node + ".output_filter",
-            node + ".output_filter_accept" : node + ".output_filter",
-            node + ".internals_in" : node + ".internals",
-            node + ".internals_out" : node + ".internals",
-            node + ".post_routing_in" : node + ".post_routing",
-            node + ".routing_in" : node + ".routing",
-            node + ".routing_out" : node + ".routing"
-        }
-
-        input_ports = {
-            node + "." + str(port) + "_ingress" : node + ".pre_routing" for port in ports
-        }
-        output_ports = {
-            node + "." + str(port) + "_egress" : node + ".post_routing" for port in ports
-        }
-
-        external_ports = { node + '.' + str(port) : "" for port in ports }
-
-        plen = len(ports)
-
-        self.ports = dict(
-            self.internal_ports.items() + input_ports.items() + output_ports.items() + external_ports.items()
-        )
-
-        self.tables[node + ".post_routing"] = [ # low priority: forward packets according to out port
-                                                # field set by the routing table
-            SwitchRule(
-                node, "post_routing", idx,
-                in_ports=[node+".post_routing_in"],
-                match=Match(
-                    fields=[SwitchRuleField("out_port", "%s.%s_egress" % (node, port))]
-                ),
-                actions=[
-                    Rewrite(rewrite=[
-                        SwitchRuleField("in_port", "x"*32),
-                        SwitchRuleField("out_port", "x"*32)
-                    ]),
-                    Forward(ports=["%s.%s_egress" % (node, port)])
-                ]
-            ) for idx, port in enumerate(ports, start=plen)
-        ] + [ # high priority: filter packets with equal input and output port
-            SwitchRule(
-                node, "post_routing", idx,
-                in_ports=[node+".post_routing_in"],
-                match=Match(
-                    fields=[
-                        SwitchRuleField("in_port", "%s.%s_ingress" % (node, port)),
-                        SwitchRuleField("out_port", "%s.%s_egress" % (node, port))
-                    ]
-                ),
-                actions=[]
-            ) for idx, port in enumerate(ports)
-        ]
-
-        self.wiring = [
-            (node + ".pre_routing_input", node + ".input_filter_in"), # pre routing to input filter
-            (node + ".input_filter_accept", node + ".internals_in"), # input filter accept to internals
-            (node + ".pre_routing_forward", node + ".forward_filter_in"), # pre routing to forward filter
-            (node + ".forward_filter_accept", node + ".routing_in"), # forward filter accept to routing
-            (node + ".internals_out", node + ".output_filter_in"), # internal output to output filter
-            (node + ".output_filter_accept", node + ".routing_in"), # output filter accept to routing
-            (node + ".routing_out", node + ".post_routing_in") # routing to post routing
-        ]
-
-        if address:
-            self.set_address(address)
 
 
     def __sub__(self, other):
         assert self.node == other.node
         assert self.type == other.type
 
-        npf = PacketFilterModel(
+        npf = AbstractPacketFilterModel(
             self.node,
+            self.type,
             ports=self.ports
         )
         npf.tables = copy(self.adds)
@@ -149,7 +67,7 @@ class PacketFilterModel(Model):
 
     def __str__(self):
         return "%s\nrules:\n\t%s\nchains:\n\t%s\nports:\n\t%s" % (
-            super(PacketFilterModel, self).__str__(),
+            super(AbstractPacketFilterModel, self).__str__(),
             str(self.chains),
             str(self.ports)
         )
@@ -250,9 +168,9 @@ class PacketFilterModel(Model):
         rule_wrong_io.tid = self.node+".routing"
         rule.tid = self.node+".routing"
 
-        super(PacketFilterModel, self).add_rule(rule_exact)
-        super(PacketFilterModel, self).add_rule(rule_wrong_io)
-        super(PacketFilterModel, self).add_rule(rule)
+        super(AbstractPacketFilterModel, self).add_rule(rule_exact)
+        super(AbstractPacketFilterModel, self).add_rule(rule_wrong_io)
+        super(AbstractPacketFilterModel, self).add_rule(rule)
 
 
     def remove_rule(self, idx):
@@ -261,7 +179,7 @@ class PacketFilterModel(Model):
         Keyword arguments:
         idx -- a rule index
         """
-        super(SwitchModel, self).remove_rule(idx)
+        super(AbstractPacketFilterModel, self).remove_rule(idx)
         del self.tables[self.node+".routing"][rule_exact.idx]
         del self.tables[self.node+".routing"][rule_wrong_io.idx]
         del self.tables[self.node+".routing"][rule.idx]
@@ -280,6 +198,100 @@ class PacketFilterModel(Model):
 
         self.remove_rule(idx)
         self.add_rule(idx, rule)
+
+
+class PacketFilterModel(AbstractPacketFilterModel):
+    """ This class stores packet filter models.
+    """
+
+    def __init__(self, node, ports=None, address=None):
+        super(PacketFilterModel, self).__init__(node, "packet_filter")
+
+        ports = ports if ports is not None else ["1", "2"]
+
+        self.tables = {
+            node + ".pre_routing" : [],
+            node + ".input_filter" : [],
+            node + ".output_filter" : [],
+            node + ".forward_filter" : [],
+            node + ".routing" : [],
+            node + ".post_routing" : [],
+            node + ".internals" : [],
+        }
+
+        self.internal_ports = {
+            node + ".pre_routing_input" : node + ".pre_routing",
+            node + ".pre_routing_forward" : node + ".pre_routing",
+            node + ".input_filter_in" : node + ".input_filter",
+            node + ".input_filter_accept" : node + ".input_filter",
+            node + ".forward_filter_in" : node + ".forward_filter",
+            node + ".forward_filter_accept" : node + ".forward_filter",
+            node + ".output_filter_in" : node + ".output_filter",
+            node + ".output_filter_accept" : node + ".output_filter",
+            node + ".internals_in" : node + ".internals",
+            node + ".internals_out" : node + ".internals",
+            node + ".post_routing_in" : node + ".post_routing",
+            node + ".routing_in" : node + ".routing",
+            node + ".routing_out" : node + ".routing"
+        }
+
+        input_ports = {
+            node + "." + str(port) + "_ingress" : node + ".pre_routing" for port in ports
+        }
+        output_ports = {
+            node + "." + str(port) + "_egress" : node + ".post_routing" for port in ports
+        }
+
+        external_ports = { node + '.' + str(port) : "" for port in ports }
+
+        plen = len(ports)
+
+        self.ports = dict(
+            self.internal_ports.items() + input_ports.items() + output_ports.items() + external_ports.items()
+        )
+
+        self.tables[node + ".post_routing"] = [ # low priority: forward packets according to out port
+                                                # field set by the routing table
+            SwitchRule(
+                node, "post_routing", idx,
+                in_ports=[node+".post_routing_in"],
+                match=Match(
+                    fields=[SwitchRuleField("out_port", "%s.%s_egress" % (node, port))]
+                ),
+                actions=[
+                    Rewrite(rewrite=[
+                        SwitchRuleField("in_port", "x"*32),
+                        SwitchRuleField("out_port", "x"*32)
+                    ]),
+                    Forward(ports=["%s.%s_egress" % (node, port)])
+                ]
+            ) for idx, port in enumerate(ports, start=plen)
+        ] + [ # high priority: filter packets with equal input and output port
+            SwitchRule(
+                node, "post_routing", idx,
+                in_ports=[node+".post_routing_in"],
+                match=Match(
+                    fields=[
+                        SwitchRuleField("in_port", "%s.%s_ingress" % (node, port)),
+                        SwitchRuleField("out_port", "%s.%s_egress" % (node, port))
+                    ]
+                ),
+                actions=[]
+            ) for idx, port in enumerate(ports)
+        ]
+
+        self.wiring = [
+            (node + ".pre_routing_input", node + ".input_filter_in"), # pre routing to input filter
+            (node + ".input_filter_accept", node + ".internals_in"), # input filter accept to internals
+            (node + ".pre_routing_forward", node + ".forward_filter_in"), # pre routing to forward filter
+            (node + ".forward_filter_accept", node + ".routing_in"), # forward filter accept to routing
+            (node + ".internals_out", node + ".output_filter_in"), # internal output to output filter
+            (node + ".output_filter_accept", node + ".routing_in"), # output filter accept to routing
+            (node + ".routing_out", node + ".post_routing_in") # routing to post routing
+        ]
+
+        if address:
+            self.set_address(address)
 
 
     @staticmethod
