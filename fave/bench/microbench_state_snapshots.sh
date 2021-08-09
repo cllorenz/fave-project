acb45cc6b94f03c7cba378da686ecec55455744f
Claas Lorenz <cllorenz@uni-potsdam.de>
2021-08-09 21:33:37 +0200

    Microbench with fewer hz for policy run.

--- fave/bench/microbench_state_snapshots.sh
+++ fave/bench/microbench_state_snapshots.sh
@@ -18,20 +18,28 @@ echo "" >> $FWOP
 echo "" >> $FWIP
 
 export PYTHONPATH=.
-for hz in 1 10 100 200 300 400 500 600 700 800 900 1000; do
+echo "run without policies"
+for hz in 1 10 50 100 200 300 400 500 600 700 800 900 1000; do
 	echo -n "$hz"
 	echo -n "$hz" >> $FWOP
-	echo -n "$hz" >> $FWIP
 	for i in $(seq 1 $RUNS); do
 		RES=`python2 bench/wl_state_snapshots/benchmark.py -f $hz | cut -d ' ' -f 3`
+		echo -n " $RES"
 		echo -n " $RES" >> $FWOP
+	done
+	echo ""
+	echo "" >> $FWOP
+done
 
+echo "run with policies"
+for hz in 1 10 50 100; do
+	echo -n "$hz"
+	echo -n "$hz" >> $FWIP
+	for i in $(seq 1 $RUNS); do
 		RES=`python2 bench/wl_state_snapshots/benchmark.py -p -f $hz | cut -d ' ' -f 3`
 		echo -n " $RES"
 		echo -n " $RES" >> $FWIP
 	done
 	echo ""
-	echo "" >> $FWOP
 	echo "" >> $FWIP
 done
-
