a46ca97a9ecbbf293236618e73f26367297ebf55
Claas Lorenz <clorenz@genua.de>
2021-12-10 16:43:11 +0100

    Some more licensing.

--- fave/bench/np_preparation.py
+++ fave/bench/np_preparation.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python2
 
+# -*- coding: utf-8 -*-
+
+# Copyright 2021 Claas Lorenz <claas_lorenz@genua.de>
+
+# This file is part of FaVe.
+
+# FaVe is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# FaVe is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with FaVe.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import json
 
 from netplumber.mapping import FIELD_SIZES
