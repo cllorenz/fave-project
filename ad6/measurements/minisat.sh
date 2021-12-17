3743b19eebd13c391575129a3d165f0ef1889cf4
Claas Lorenz <clorenz@genua.de>
2021-12-17 12:52:09 +0100

    Remove ad6 from repo for now.

--- ad6/measurements/minisat.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env sh
-
-minisat /dev/shm/solver.in | grep "CPU time" | tr -s [[:blank:]] '\t' | cut -f 4
--- ad6/results/convert.pdf.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-
-gnuplot $1.p
-
-epstopdf $1.ps
-pdfcrop $1.pdf > /dev/null
-mv $1-crop.pdf $1.pdf
