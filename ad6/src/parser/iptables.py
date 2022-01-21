e52791fcd3f40a2854d2c2b6f495f13fb1a19ee8
Claas Lorenz <clorenz@genua.de>
2022-01-21 11:08:23 +0100

    Set globally unique keys for rules while parsing.

--- ad6/src/parser/iptables.py
+++ ad6/src/parser/iptables.py
@@ -53,16 +53,23 @@ class IP6TablesParser:
         chains = {}
         chain_defaults = {}
         rule_counts = {}
+        line_count = 1
+
         for line in [l for l in ruleset.splitlines() if l and not l.startswith('#')]:
             tokens = line.split()
+
             ip_version = '6' if tokens[0] == 'ip6tables' else '4'
+
             args = parser.parse_args(tokens[1:])
             if args.policy:
                 chain, target = args.policy
                 chain = chain.lower()
+                rule_counts.setdefault(chain, 0)
                 target = target.lower()
                 chains.setdefault(chain, GenUtils.table(chain))
-                chain_defaults.setdefault(chain, (target, line))
+                chain_defaults.setdefault(chain, (target, line, rule_counts[chain]))
+                rule_counts[chain] += 1
+                line_count += 1
                 continue
 
             else:
@@ -71,20 +78,22 @@ class IP6TablesParser:
                 target = args.target.lower()
 
             rule_counts.setdefault(chain, 0)
-            raw_line_no = rule_counts[chain]
-            idx = rule_counts[chain] << 12
+            raw_line_no = line_count
+
+            idx = (rule_counts[chain] - 1) << 12
+            key = "%s_%s_r%d" % (fw_name, chain, idx)
             rule = GenUtils.rule(
                 str(idx),
-                key="%s_%s_r%d"%(fw_name, chain, idx),
+                key=key,
                 raw=line,
                 raw_line_no=raw_line_no
             )
 
             extended.setdefault(chain, {})
-            extended[chain].setdefault(raw_line_no, [])
-            extended[chain][raw_line_no].append(idx)
+            extended[chain].setdefault(key, [])
+            extended[chain][key].append(key)
             original.setdefault(chain, {})
-            original[chain][idx] = raw_line_no
+            original[chain][key] = key
 
             rule.append(GenUtils.action('jump', "%s_%s_r0"%(fw_name, target)))
 
@@ -135,15 +144,14 @@ class IP6TablesParser:
                 for i, prefixed_port in enumerate(prefixed_ports):
                     port, prefix = prefixed_port
                     tmp = deepcopy(rule)
-                    tmp.attrib['name'] = 'r'+str(idx+i)
-                    tmp.attrib['key'] = "%s_%s_r%d" % (fw_name, chain, idx+i)
+                    tmp_idx = idx+(i<<6)
+                    tmp_key = "%s_%s_r%d" % (fw_name, chain, tmp_idx)
+                    tmp.attrib['name'] = 'r'+str(tmp_idx)
+                    tmp.attrib['key'] = tmp_key
                     tmp.append(GenUtils.port("%s/%s"%(port, prefix), 'src'))
                     rules.append(tmp)
-                    extended.setdefault(chain, {})
-                    extended[chain].setdefault(raw_line_no, [])
-                    extended[chain][raw_line_no].append(idx+i)
-                    original.setdefault(chain, {})
-                    original[chain][idx+i] = raw_line_no
+                    extended[chain][key].append(tmp_key)
+                    original[chain][tmp_key] = key
 
             if args.dst_ports:
                 start, end = args.dst_ports.split(':')
@@ -155,15 +163,15 @@ class IP6TablesParser:
                     port, prefix = prefixed_port
                     for j, rule in enumerate(tmp_rules):
                         tmp = deepcopy(rule)
-                        tmp.attrib['name'] = 'r'+str(idx+i+j)
-                        tmp.attrib['key'] = "%s_%s_r%d" % (fw_name, chain, idx+i+j)
+                        tmp_idx = idx+(j<<6)+i
+                        tmp_key = "%s_%s_r%d" % (fw_name, chain, tmp_idx)
+                        tmp.attrib['name'] = 'r'+str(tmp_idx)
+                        tmp.attrib['key'] = tmp_key
                         tmp.append(GenUtils.port("%s/%s"%(port, prefix), 'dst'))
                         rules.append(tmp)
-                        extended.setdefault(chain, {})
-                        extended[chain].setdefault(raw_line_no, [])
-                        extended[chain][raw_line_no].append(idx+i+j)
+                        extended[chain][key].append(tmp_key)
                         original.setdefault(chain, {})
-                        original[chain][idx+i+j] = raw_line_no
+                        original[chain][tmp_key] = key
 
             if rules:
                 chains[chain].extend(rules)
@@ -171,29 +179,31 @@ class IP6TablesParser:
                 chains[chain].append(rule)
 
             rule_counts[chain] += 1
+            line_count += 1
 
         for chain, default in chain_defaults.items():
-            target, line = default
-            idx = rule_counts[chain] << 12
-            rule = GenUtils.rule(str(idx), key="%s_%s_r%d"%(fw_name, chain, idx), raw=line, raw_line_no=65535)
+            target, line, lineno = default
+            idx = ((rule_counts[chain] - len(chain_defaults) + lineno) << 12)
+            key = "%s_%s_r%d" % (fw_name, chain, idx)
+            rule = GenUtils.rule('r'+str(idx), key=key, raw=line, raw_line_no=lineno)
             rule.append(GenUtils.action("jump", target="%s_%s_r0"%(fw_name, target)))
             chains[chain].append(rule)
             extended.setdefault(chain, {})
-            extended[chain].setdefault(65535, [])
-            extended[chain][65535].append(idx)
+            extended[chain].setdefault(key, [])
+            extended[chain][key].append(key)
             original.setdefault(chain, {})
-            original[chain][idx] = 65535
+            original[chain][key] = key
 
         firewall = GenUtils.firewall(fw_name)
 
         accept = GenUtils.table('accept')
-        rule = GenUtils.rule('0', key="%s_accept_r0"%fw_name)
+        rule = GenUtils.rule('r0', key="%s_accept_r0"%fw_name)
         rule.append(GenUtils.action('accept'))
         accept.append(rule)
         firewall.append(accept)
 
         drop = GenUtils.table('drop')
-        rule = GenUtils.rule('0', key="%s_drop_r0"%fw_name)
+        rule = GenUtils.rule('r0', key="%s_drop_r0"%fw_name)
         rule.append(GenUtils.action('drop'))
         drop.append(rule)
         firewall.append(drop)
