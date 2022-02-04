69e9ee750a6b77639d4c541485b3620635bc1dc8
Claas Lorenz <cllorenz@uni-potsdam.de>
2022-02-04 12:12:19 +0100

    Add script to run TUM benchmark with z3.

---
+++ z3-anomalies/z3.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+RES=results/z3
+
+mkdir -p $RES
+rm -rf $RES/*
+
+echo -n "run benchmarks:"
+for i in {1..10}; do
+    echo -n " $i"
+    python3 main.py -r rulesets/tum-ruleset > $RES/$i.stdout.log
+done
+echo ""
