9971fba400a068d41aa2fc1e5ac94bc2d7cf9f54
Claas Lorenz <clorenz@genua.de>
2022-02-07 11:38:40 +0100

    Add script to evaluate shadowing measurements.

---
+++ fave/bench/eval_shadow.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+RES=$1
+
+AD6_RAW=$RES/ad6_raw.dat
+Z3_RAW=$RES/z3_raw.dat
+NP_RAW=$RES/np_raw.dat
+
+RESULT=$RES/result.dat
+
+echo -n "" > $AD6_RAW
+echo -n "" > $Z3_RAW
+echo -n "" > $NP_RAW
+
+# analyse ad6
+grep "Solving" $RES/ad6/*.stdout.log | cut -f7 >> $AD6_RAW
+
+# analyse z3
+grep "Analysis" $RES/z3/*.stdout.log | cut -d' ' -f3 >> $Z3_RAW
+
+# analyse np
+grep "Checked anomalies" $RES/np/*.stdout.log | cut -d' ' -f7 >> $NP_RAW
+
+echo -e "tool\tmean\tmedian\tstddev" > $RESULT
+
+for T in ad6:$AD6_RAW Z3:$Z3_RAW NetPlumber:$NP_RAW; do
+    echo $T
+
+    TOOL=$(echo $T | cut -d: -f1)
+    RAW=$(echo $T | cut -d: -f2)
+
+    MEAN=`awk -f bench/mean.awk < $RAW`
+    MEDIAN=`awk -f bench/median.awk < $RAW`
+    STDDEV=`awk -f bench/stddev.awk -vMEAN=$MEAN < $RAW`
+
+    echo -e "$TOOL\t$MEAN\t$MEDIAN\t$STDDEV" >> $RESULT
+done
