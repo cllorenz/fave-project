3256be57e40e204fa788566249e4bc327f8713cd
Claas Lorenz <cllorenz@uni-potsdam.de>
2022-02-25 13:47:36 +0100

    Script for UP measurements.

--- z3-anomalies/z3_tum.sh
+++ z3-anomalies/z3_tum.sh
@@ -8,6 +8,6 @@ rm -rf $RES/*
 echo -n "run benchmarks:"
 for i in {1..10}; do
     echo -n " $i"
-    python3 main.py -r rulesets/tum-ruleset > $RES/$i.stdout.log
+    python3 main.py -m -r rulesets/tum-ruleset > $RES/$i.stdout.log
 done
 echo ""
---
+++ z3-anomalies/z3_up.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+RES=results_up/z3
+
+mkdir -p $RES
+rm -rf $RES/*
+
+echo -n "run benchmarks:"
+for i in {1..10}; do
+    echo -n " $i"
+    python3 main.py -m -r rulesets/up-ruleset > $RES/$i.stdout.log
+done
+echo ""
