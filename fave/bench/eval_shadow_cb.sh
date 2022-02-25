c5a41d7e05a397c470d2e2b369fedddb1ac48559
Claas Lorenz <clorenz@genua.de>
2022-02-25 10:01:13 +0100

    Refactor benchmark script.

--- fave/bench/eval_shadow_cb.sh
+++ fave/bench/eval_shadow_cb.sh
@@ -2,24 +2,27 @@
 
 RES=$1
 
-for fw in fw1 fw2 fw3 fw4 fw5 random; do
-    for c in 500 1000 2000; do
+RULESETS="fw1 fw2 fw3 fw4 fw5 random"
+RULES="500 1000 2000 5000 10000"
+
+for fw in $RULESETS; do
+    for c in $RULES; do
         echo -n "" > $RES/$fw"_"$c"_raw.dat"
         echo -n "" > $RES/$fw".dat"
     done
 done
 
-for fw in fw1 fw2 fw3 fw4 fw5 random; do
-    for c in 500 1000 2000; do
+for fw in $RULESETS; do
+    for c in $RULES; do
         RAW=$RES/$fw"_"$c"_raw.dat"
         grep "check_anomalies" $RES/$fw/$c/*_fave.log | cut -d' ' -f6 >> $RAW
     done
 done
 
-for fw in fw1 fw2 fw3 fw4 fw5 random; do
+for fw in $RULESETS; do
     FW_RESULT=$RES/$fw".dat"
     echo -e "rules\tmean\tmedian\tstddev" > $FW_RESULT
-    for c in 500 1000 2000; do
+    for c in $RULES; do
         RAW=$RES/$fw"_"$c"_raw.dat"
 
         MEAN=`awk -f bench/mean.awk < $RAW`
