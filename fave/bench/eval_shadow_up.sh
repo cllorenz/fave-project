80e9f2f04257e9680ecfc2e8bd39fa022df38bb2
Claas Lorenz <clorenz@genua.de>
2022-07-11 09:50:33 +0200

    Update analysis scripts with BV measurements.

--- fave/bench/eval_shadow_up.sh
+++ fave/bench/eval_shadow_up.sh
@@ -4,12 +4,14 @@ RES=$1
 
 AD6_RAW=$RES/ad6_raw.dat
 Z3_RAW=$RES/z3_raw.dat
+BV_RAW=$RES/bv_raw.dat
 NP_RAW=$RES/np_raw.dat
 
 RESULT=$RES/result.dat
 
 echo -n "" > $AD6_RAW
 echo -n "" > $Z3_RAW
+echo -n "" > $BV_RAW
 echo -n "" > $NP_RAW
 
 # analyse ad6
@@ -18,12 +20,15 @@ grep "Solving" $RES/ad6/*.stdout.log | cut -f7 >> $AD6_RAW
 # analyse z3
 grep "Analysis" $RES/z3/*.stdout.log | cut -d' ' -f3 >> $Z3_RAW
 
+# analyse bv
+grep "Analysis" $RES/bv/*.stdout.log | cut -d' ' -f3 >> $BV_RAW
+
 # analyse np
 grep "Checked anomalies" $RES/np/*.stdout.log | cut -d' ' -f7 >> $NP_RAW
 
 echo -e "tool\tmean\tmedian\tstddev" > $RESULT
 
-for T in ad6:$AD6_RAW Z3:$Z3_RAW FaVe:$NP_RAW; do
+for T in ad6:$AD6_RAW Z3:$Z3_RAW BV:$BV_RAW FaVe:$NP_RAW; do
     echo $T
 
     TOOL=$(echo $T | cut -d: -f1)
