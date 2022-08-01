be42d9c5c3458c6e09b8060c5176889c7779a9f0
Claas Lorenz <clorenz@genua.de>
2022-08-01 11:48:08 +0200

    Run script for small UP benchmark.

---
+++ ad6/bench/up/run_small.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+RSPATH="bench/up"
+RULESETS="up_client_fw:$RSPATH/small-client-ruleset,up_server_fw:$RSPATH/small-server-ruleset"
+ANOMALIES="reach"
+
+export PYTHONPATH=.
+python3 main.py \
+    --no-active-interfaces \
+    --network bench/up/small.xml \
+    --rulesets $RULESETS \
+    --anomalies $ANOMALIES
+
